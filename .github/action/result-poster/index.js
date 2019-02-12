a5097ad630a3b31410dc3ca5736648bbf63fa2c2
Myles Borins <mylesborins@google.com>
2019-02-12 11:37:25 -0800

    build: Move to official domain.

--- .github/action/result-poster/index.js
+++ .github/action/result-poster/index.js
@@ -9,6 +9,6 @@ const prs = context.payload.check_suite.pull_requests;
 if (name === 'Google Cloud Build' && conclusion === 'success' && prs[0]) {
   request('POST /repos/:owner/:repo/issues/:number/comments', context.repo({
     number: prs[0].number,
-    body: `Preview at: https://storage.googleapis.com/nodejs-dev-staging/${sha.slice(0,7)}/index.html`
+    body: `Preview at: https://staging.nodejs.dev/${sha.slice(0,7)}/index.html`
   }));
 }
