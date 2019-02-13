04526d10515e6b2c324426a1465ea7c14ec4d98d
mbj36 <mohitbajo36@gmail.com>
2019-02-14 00:30:30 +0530

    chore: Add links to navbar.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from 'react'
+import { Link } from 'gatsby'
 
-import logo from '../images/logo.svg';
+import logo from '../images/logo.svg'
 
 type Props = {
   siteTitle: string
@@ -20,8 +20,30 @@ const Header = ({ siteTitle }: Props) => (
     >
       <li>
         <Link to="/">
-          <img src={logo} alt={siteTitle} style={{ height: '62px', margin: '-6px 24px 0 0' }} />
+          <img
+            src={logo}
+            alt={siteTitle}
+            style={{ height: '62px', margin: '-6px 24px 0 0' }}
+          />
         </Link>
+      </li>
+      <li style={{ paddingLeft: '2rem', marginBottom: '12px' }}>
+        <a
+          target="_blank"
+          style={{ textDecoration: 'none' }}
+          href="https://nodejs.org/en/docs/"
+        >
+          API Docs
+        </a>
+      </li>
+      <li style={{ paddingLeft: '2rem', marginBottom: '12px' }}>
+        <a
+          target="_blank"
+          style={{ textDecoration: 'none' }}
+          href="https:github.com/nodejs/website-prototype"
+        >
+          GitHub
+        </a>
       </li>
     </ul>
   </nav>
