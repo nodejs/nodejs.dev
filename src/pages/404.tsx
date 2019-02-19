92e7689c895a52a7138cd97833238b5480c6fa64
Ahmad Awais <me@AhmadAwais.com>
2019-02-20 03:59:21 +0500

    refactor: SEO 404 page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+const title = 'NOT FOUND 404'
+const description =
+  'You just hit a route that doesn&#39;t exist... the sadness.'
+
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <Layout title={title} description={description}>
+    <h1>{title}</h1>
+    <p>{description}</p>
   </Layout>
 )
 
