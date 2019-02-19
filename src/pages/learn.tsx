2eaa29c0b2e4fdc3c8bd3ad2104c147237901bf4
Ahmad Awais <me@AhmadAwais.com>
2019-02-20 04:02:55 +0500

    refactor: Add Title, Author, Desc SEO.

--- src/pages/learn.tsx
+++ src/pages/learn.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { graphql, Link } from 'gatsby';
-
-import Layout from '../components/layout';
-import { scrollTo } from '../util/scrollTo';
+import { graphql, Link } from 'gatsby'
+import React from 'react'
+import Layout from '../components/layout'
+import { scrollTo } from '../util/scrollTo'
 
 /**
  * When on the "Learn" page, we need to update the background gradient
@@ -10,27 +9,29 @@ import { scrollTo } from '../util/scrollTo';
  * as it becomes sticky and overlays the hero banner.
  * TODO: This should happen in the component, only when rendered.
  */
-let prevOffset = -1;
+let prevOffset = -1
 function magicHeroNumber() {
-  if (typeof window === 'undefined') { return; } // Guard for SSR.
-  const doc = window.document;
-  const offset = Math.min(doc.scrollingElement!.scrollTop, 210);
+  if (typeof window === 'undefined') {
+    return
+  } // Guard for SSR.
+  const doc = window.document
+  const offset = Math.min(doc.scrollingElement!.scrollTop, 210)
   if (Math.abs(prevOffset - offset) > 5) {
-    prevOffset = offset;
-    doc.body.setAttribute('style', `--magic-hero-number: ${365 - offset}px`);
+    prevOffset = offset
+    doc.body.setAttribute('style', `--magic-hero-number: ${365 - offset}px`)
   }
-  window.requestAnimationFrame(magicHeroNumber);
+  window.requestAnimationFrame(magicHeroNumber)
 }
-magicHeroNumber();
+magicHeroNumber()
 
 interface RemarkPage {
-  id: string;
-  fileAbsolutePath: string;
-  html: string;
+  id: string
+  fileAbsolutePath: string
+  html: string
   frontmatter: {
-    title: string;
-    description: string;
-    author: string;
+    title: string
+    description: string
+    author: string
   }
 }
 
@@ -48,37 +49,42 @@ interface NextPage {
 
 interface LearnPageData {
   pages: {
-    edges: ({ node: RemarkPage, previous: PreviousPage, next: NextPage })[];
+    edges: ({ node: RemarkPage; previous: PreviousPage; next: NextPage })[]
   }
 }
 type Props = {
-  data: LearnPageData;
-  location: Location;
+  data: LearnPageData
+  location: Location
 }
 
-let prev = 0;
+let prev = 0
 function openNav() {
-  const old = prev;
-  prev = document.scrollingElement!.scrollTop;
-  document.getElementsByClassName('side-nav')[0].classList.toggle('side-nav--open');
-  scrollTo(old);
+  const old = prev
+  prev = document.scrollingElement!.scrollTop
+  document
+    .getElementsByClassName('side-nav')[0]
+    .classList.toggle('side-nav--open')
+  scrollTo(old)
 }
-/** Small screen width 
+/** Small screen width
  *  If the width of the viewport is lesser than this value
  * it means that the website is viewed in a tablet or mobile
  * TODO have this number shared in one place in the project
  */
-const MAX_SMALLSCREEN_WIDTH = 1262;
+const MAX_SMALLSCREEN_WIDTH = 1262
 /**
  * When on smaller devices such as tablets and mobiles, the side menu needs
  * to be close when an article is selected.
  * closeNavOnSmallScreens checks for the viewport width and toggles it sideNav
- * if it is open on a small screen 
+ * if it is open on a small screen
  */
 function closeNavOnSmallScreens() {
-  // Get viewport width 
+  // Get viewport width
   // Source - https://stackoverflow.com/a/8876069/2621400
-  const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  const w = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  )
   // If width is lesser or equal to max small screen width
   if (w <= MAX_SMALLSCREEN_WIDTH) {
     openNav()
@@ -86,41 +92,72 @@ function closeNavOnSmallScreens() {
 }
 
 export default ({ data, location }: Props) => {
-  const pages = [];
-  let currentPage = location.pathname.split('/').pop();
-  let activePage = { title: '404', html: '404' };
+  const pages = []
+  let currentPage = location.pathname.split('/').pop()
+  let activePage = { title: '404', html: '404', author: '', description: '' }
 
   let previousPage = { frontmatter: { title: '404', path: '404' } }
   let nextPage = { frontmatter: { title: '404', path: '404' } }
 
-  
-  let foundActive = false;
+  let foundActive = false
 
   // For every page,
   for (const { node: page, previous, next } of data.pages.edges) {
     // If this page does not have a title, skip
-    if (!page.frontmatter.title) { continue; }
+    if (!page.frontmatter.title) {
+      continue
+    }
 
     // Generate a slug for this page
     // TODO: We need a more robust slug creation here.
-    const slug = page.frontmatter.title.toLowerCase().replace(/ /g, '-').replace(/[^a-z|-]/g, '');
+    const slug = page.frontmatter.title
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .replace(/[^a-z|-]/g, '')
 
     // If there is no current page slug discovered from the URL, use the first page's.
-    if (!currentPage) { currentPage = slug; }
+    if (!currentPage) {
+      currentPage = slug
+    }
 
     // Determine if this is the active page, and mark if we've found the active page yet.
-    const isActive = slug === currentPage;
-    foundActive = foundActive || isActive;
+    const isActive = slug === currentPage
+    foundActive = foundActive || isActive
     if (isActive) {
-      activePage = { html: page.html, title: page.frontmatter.title };
+      activePage = {
+        html: page.html,
+        title: page.frontmatter.title,
+        description: page.frontmatter.description,
+        author: page.frontmatter.author,
+      }
 
-      previousPage = { frontmatter: { title: previous.frontmatter.title, path: previous.frontmatter.title.toLowerCase().replace(/ /g, '-').replace(/[^a-z|-]/g, '') } };
+      previousPage = {
+        frontmatter: {
+          title: previous.frontmatter.title,
+          path: previous.frontmatter.title
+            .toLowerCase()
+            .replace(/ /g, '-')
+            .replace(/[^a-z|-]/g, ''),
+        },
+      }
 
-      nextPage = { frontmatter: { title: next && next.frontmatter.title, path: next && next.frontmatter.title.toLowerCase().replace(/ /g, '-').replace(/[^a-z|-]/g, '') } };
+      nextPage = {
+        frontmatter: {
+          title: next && next.frontmatter.title,
+          path:
+            next &&
+            next.frontmatter.title
+              .toLowerCase()
+              .replace(/ /g, '-')
+              .replace(/[^a-z|-]/g, ''),
+        },
+      }
     }
 
     // Construct class name for this side nav item.
-    const className = `side-nav__item ${!foundActive ? 'side-nav__item--done' : ''} ${isActive ? 'side-nav__item--active' : ''}`;
+    const className = `side-nav__item ${
+      !foundActive ? 'side-nav__item--done' : ''
+    } ${isActive ? 'side-nav__item--active' : ''}`
 
     // Add the constructed page JSX to the pages list.
     pages.push(
@@ -129,11 +166,14 @@ export default ({ data, location }: Props) => {
           {page.frontmatter.title}
         </Link>
       </li>
-    );
+    )
   }
 
   return (
-    <Layout>
+    <Layout
+      title={`${activePage.title} by ${activePage.author}`}
+      description={activePage.description}
+    >
       <div className="hero">
         <h1>{activePage.title}</h1>
         <div className="diagonal-hero-bg">
@@ -145,7 +185,9 @@ export default ({ data, location }: Props) => {
         </div>
       </div>
       <nav className="side-nav">
-        <button className="side-nav__open" onClick={openNav}>Menu</button>
+        <button className="side-nav__open" onClick={openNav}>
+          Menu
+        </button>
         {/* TODO: Get side nav sections from frontmatter fields. Need a new one. */}
         {/* TODO: H2s should not be in the ULs, but needed to make sticky titles work. Find a new way. */}
         <ul className="side-nav__list">
@@ -167,51 +209,53 @@ export default ({ data, location }: Props) => {
             justifyContent: `space-between`,
             listStyle: `none`,
             padding: '30px',
-           
           }}
         >
           <li>
-            {previousPage.frontmatter.title &&
+            {previousPage.frontmatter.title && (
               <Link to={`/learn/${previousPage.frontmatter.path}`} rel="prev">
-                ←  &nbsp; Prev
+                ← &nbsp; Prev
               </Link>
-            }
-        </li>
-          <li>
-            {nextPage.frontmatter.title && <Link to={`/learn/${nextPage.frontmatter.path}`} rel="next">
-              Next &nbsp; →
-          </Link>}
+            )}
           </li>
-          </ul>
+          <li>
+            {nextPage.frontmatter.title && (
+              <Link to={`/learn/${nextPage.frontmatter.path}`} rel="next">
+                Next &nbsp; →
+              </Link>
+            )}
+          </li>
+        </ul>
       </article>
-      
     </Layout>
-  );
+  )
 }
 
-export const query = graphql`{
-  pages: allMarkdownRemark {
-    edges {
-      node {
-        id,
-        fileAbsolutePath,
-        html,
-        frontmatter {
-          title
-          description
-          author
+export const query = graphql`
+  {
+    pages: allMarkdownRemark {
+      edges {
+        node {
+          id
+          fileAbsolutePath
+          html
+          frontmatter {
+            title
+            description
+            author
+          }
         }
-      }
-      next {
-        frontmatter {
-          title
+        next {
+          frontmatter {
+            title
+          }
         }
-      }
-      previous {
-        frontmatter {
-          title
+        previous {
+          frontmatter {
+            title
+          }
         }
       }
     }
   }
-}`;
+`
