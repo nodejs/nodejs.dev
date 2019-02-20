5c47418d777b39e62a20c2e7961c288d3eafd282
Ahmad Awais <me@AhmadAwais.com>
2019-02-21 00:03:32 +0500

    feat: Add common config file.

---
+++ src/config.js
@@ -0,0 +1,23 @@
+/**
+ * Common App Configurations.
+ */
+
+module.exports = {
+  title: `Node.js`,
+  siteUrl: `https://nodejs.dev/`,
+  siteUrlNoSlash: `https://vscode.pro`,
+  description: `Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine.`,
+  featuredImage: `https://nodejs.org/static/images/logo-hexagon-card.png`,
+  color: `#6cc24a`,
+  icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+  lang: `en`,
+  display: `minimal-ui`,
+  ogType: `website`,
+  ogImgType: `image/png`,
+  ogImgWidth: `224`,
+  ogImgHeight: `256`,
+  twitter: `@nodejs`,
+  twitterCard: `summary`,
+  twitterImgAlt: `The Node.js Hexagon Logo`,
+  facebook: `https://www.facebook.com/nodejsfoundation/`,
+}
