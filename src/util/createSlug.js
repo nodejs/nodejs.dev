02e42c57feb14b788d248dbd4b10abcdd1833004
Olle Lauri Boström <ollebostr@gmail.com>
2019-02-20 08:17:52 +0100

    feat: Improve slug generation. (#58)

---
+++ src/util/createSlug.js
@@ -0,0 +1,19 @@
+function createSlug(title) {
+  let slug = title.toLowerCase().trim();
+  
+  const sets = [
+    {to: 'nodejs', from: /node.js/ }, // Replace node.js
+    {to: '-and-', from: /&/ }, // Replace &
+    {to: '-', from: /(\/|_|,|:|;|\\|\ |\.)/g } // Replace /_,:;\. and whitespace
+  ];
+  
+  sets.forEach(set => {
+    slug = slug.replace(set.from, set.to);
+  });
+
+  return slug
+    .replace(/[^\w\-]+/g, '') // Remove any non word characters
+    .replace(/\-\-+/g, '-');  // Replace multiple hyphens
+}
+
+module.exports = createSlug;
