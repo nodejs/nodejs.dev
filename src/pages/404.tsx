1cef2b95a65f984a7fe7b5fb22e4232df98c589e
KirankumarAmbati <kiran.chinna12520@gmail.com>
2019-03-03 12:08:44 +0530

    closes #165: fix for navigation on 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { graphql } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 import React from 'react';
 import Hero from '../components/hero';
 import Layout from '../components/layout';
@@ -6,38 +6,42 @@ import Navigation from '../components/navigation';
 import { LearnPageData } from '../types';
 import { findActive } from '../util/findActive';
 
-type Props = {
-  data: LearnPageData;
-};
-
-export default ({ data }: Props) => {
+export default () => {
   // Get current page related data.
-  const { navigationSections } = findActive(data.sections.group);
-
   const title = `PAGE NOT FOUND`;
   const description = `You've hit a route that does not exist.`;
 
   return (
-    <Layout title={title} description={description}>
-      <Hero title={title} />
-      <Navigation sections={navigationSections} />
-      <article className="article-reader">
-        <h1 className="article-reader__headline">{title}</h1>
-        <div>
-          <p>
-            The page you're trying to access does not exist. Go back to the
-            Homepage or find what you're looking for in the menu.
-          </p>
-          <p>
-            Take me back to the <a href="/">Homepage</a> →
-          </p>
-        </div>
-      </article>
-    </Layout>
+    <StaticQuery
+      query={query}
+      render={data => {
+
+        const { navigationSections } = findActive(data.sections.group);
+
+        return (
+          <Layout title={title} description={description}>
+            <Hero title={title} />
+            <Navigation sections={navigationSections} />
+            <article className="article-reader">
+              <h1 className="article-reader__headline">{title}</h1>
+              <div>
+                <p>
+                  The page you're trying to access does not exist. Go back to the
+                  Homepage or find what you're looking for in the menu.
+                </p>
+                <p>
+                  Take me back to the <a href="/">Homepage</a> →
+                </p>
+              </div>
+            </article>
+          </Layout>
+        )
+      }}
+    />
   );
 };
 
-export const query = graphql`
+const query = graphql`
   {
     sections: allMarkdownRemark(
       sort: { fields: [fileAbsolutePath], order: ASC }
