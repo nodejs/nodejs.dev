8e9944ac5003bfe2c6822e032b572c6f0363f4c5
Beni von Cheni <benjaminlchen@gmail.com>
2019-03-03 00:13:43 -0500

    refactor: Revert code format update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,7 @@ const Hero = ({ title }: Props) => (
   <div className="hero">
     <h1>{title}</h1>
     <div className="diagonal-hero-bg">
-        <div className="stars">
+      <div className="stars">
           <div className="small"/>
           <div className="medium"/>
           <div className="big"/>
