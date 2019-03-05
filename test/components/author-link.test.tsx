9b0e89188643057b6c9355bca5fd893a1407a9c4
Onur Laru <larugit@false.one>
2019-03-05 09:03:54 +0100

    chore: Tests updated.

--- test/components/author-link.test.tsx
+++ test/components/author-link.test.tsx
@@ -5,11 +5,11 @@ import AuthorLink from '../../src/components/author-link';
 describe('AuthorLink component', () => {
   it('renders correctly', () => {
     const username = 'test-author';
-    const tree = renderer.create(<AuthorLink username={username} />).toJSON();
+    const tree = renderer.create(<AuthorLink key={username} username={username} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('does not render without a relative path', () => {
-    const tree = renderer.create(<AuthorLink username={null} />);
+  it('does not render without a username', () => {
+    const tree = renderer.create(<AuthorLink key={null} username={null} />);
     expect(tree.getInstance()).toBeNull();
   });
 });
--- test/components/pagination.test.tsx
+++ test/components/pagination.test.tsx
@@ -4,8 +4,8 @@ import Pagination from '../../src/components/pagination';
 import { createPaginationInfo } from '../__fixtures__/page';
 
 describe('Pagination component', () => {
-  const paginationInfo = createPaginationInfo();
   it('renders links to the next and previous page', () => {
+    const paginationInfo = createPaginationInfo();
     const tree = renderer
       .create(<Pagination next={paginationInfo} previous={paginationInfo} />)
       .toJSON();
@@ -13,6 +13,7 @@ describe('Pagination component', () => {
   });
 
   it('renders correctly when there is no next page', () => {
+    const paginationInfo = createPaginationInfo();
     const tree = renderer
       .create(<Pagination previous={paginationInfo} />)
       .toJSON();
@@ -20,11 +21,13 @@ describe('Pagination component', () => {
   });
 
   it('renders correctly when there is no previous page', () => {
+    const paginationInfo = createPaginationInfo();
     const tree = renderer.create(<Pagination next={paginationInfo} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('only renders links to pages that has a title', () => {
+    const paginationInfo = createPaginationInfo();
     const nextPaginationInfo = { slug: 'test-slug', title: null };
     const tree = renderer
       .create(
