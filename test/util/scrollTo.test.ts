cde43a03998e735cdb668b1189b29e557ab6cdc2
Sagir Khan <sagirk@users.noreply.github.com>
2019-03-27 13:04:51 +0530

    Update scrollTo.test.ts

--- test/util/scrollTo.test.ts
+++ test/util/scrollTo.test.ts
@@ -4,11 +4,8 @@ describe('Tests for calcNavScrollParams', () => {
   const dummyHTMLElement = document.createElement('nav');
   const height = 100; // arbritrary
   it('scrollWindow is set null for mobile devices', () => {
-    Object.defineProperty(window, 'matchMedia', {
-      value: jest.fn(() => {
-        return { matches: true };
-      }),
-    });
+    // @ts-ignore
+    window.innerWidth = 720;
     const { newScrollPos, scrollWindow, scrollTime } = calcNavScrollParams(
       height,
       dummyHTMLElement
