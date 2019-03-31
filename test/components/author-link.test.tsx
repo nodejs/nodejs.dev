e921b61ba22b3cc2ace491323f634cb51e6d6b4e
Ahmad Awais <me@AhmadAwais.com>
2019-03-31 14:29:19 +0500

    refactor: Tests

--- test/components/author-link.test.tsx
+++ test/components/author-link.test.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import AuthorLink from '../../src/components/author-link';
+import Author from '../../src/components/author';
 
 describe('AuthorLink component', () => {
   it('renders correctly', () => {
     const username = 'test-author';
     const tree = renderer
-      .create(<AuthorLink key={username} username={username} />)
+      .create(<Author username={username} size="60" />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
   it('does not render without a username', () => {
-    const tree = renderer.create(<AuthorLink key={null} username={null} />);
+    const tree = renderer.create(
+      <Author key={null} username={null} size={null} />
+    );
     expect(tree.getInstance()).toBeNull();
   });
 });
