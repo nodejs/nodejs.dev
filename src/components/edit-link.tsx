54df733159be8d7ec970740a654a11372aaadd0c
Ahmad Awais <me@AhmadAwais.com>
2019-04-03 17:30:00 +0500

    feat: Edit link with SVG Icon

--- src/components/edit-link.tsx
+++ src/components/edit-link.tsx
@@ -1,4 +1,34 @@
+import { css } from '@emotion/core';
 import React from 'react';
+
+const edit = css`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const link = css`
+  color: var(--gray7) !important;
+  text-transform: uppercase;
+  text-decoration: none !important;
+  font-size: 1.4rem;
+  font-weight: normal;
+  font-family: var(--sans-serif);
+  vertical-align: middle;
+
+  span {
+    vertical-align: middle;
+    font-weight: normal;
+  }
+
+  &:hover {
+    color: var(--brand-light) !important;
+  }
+`;
+
+const icon = css`
+  margin-left: 0.5rem;
+  vertical-align: middle;
+`;
 
 type Props = {
   relativePath?: string;
@@ -12,9 +42,20 @@ const EditLink = ({ relativePath }: Props) => {
   const href = `https://github.com/nodejs/nodejs.dev/edit/master/src/documentation/${relativePath}`;
 
   return (
-    <a style={{ display: 'inlineFlex', marginTop: '1rem' }} href={href}>
-      Edit this page on GitHub
-    </a>
+    <div css={edit}>
+      <a css={link} href={href}>
+        <span>Edit this page on GitHub</span>{' '}
+        <svg
+          css={icon}
+          fill="currentColor"
+          height="1em"
+          width="1em"
+          viewBox="0 0 40 40"
+        >
+          <path d="m34.5 11.7l-3 3.1-6.3-6.3 3.1-3q0.5-0.5 1.2-0.5t1.1 0.5l3.9 3.9q0.5 0.4 0.5 1.1t-0.5 1.2z m-29.5 17.1l18.4-18.5 6.3 6.3-18.4 18.4h-6.3v-6.2z" />
+        </svg>
+      </a>
+    </div>
   );
 };
 
