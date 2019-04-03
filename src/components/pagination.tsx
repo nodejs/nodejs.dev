4aaeee7dc16827a3419e435082d4aaf8226ec554
Ahmad Awais <me@AhmadAwais.com>
2019-04-03 17:30:26 +0500

    style: Improved Consistent CSS

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,7 +1,21 @@
+import { css, SerializedStyles } from '@emotion/core';
 import { Link } from 'gatsby';
 import React from 'react';
-import { css, SerializedStyles } from '@emotion/core';
 import { PaginationInfo } from '../types';
+
+const link: SerializedStyles = css`
+  color: var(--gray7) !important;
+  text-transform: uppercase;
+  text-decoration: none !important;
+  font-size: 1.4rem;
+  font-weight: normal;
+  font-family: var(--sans-serif);
+  vertical-align: middle;
+
+  &:hover {
+    color: var(--brand-light) !important;
+  }
+`;
 
 type Props = {
   previous?: PaginationInfo;
@@ -13,21 +27,21 @@ const ulStyles: SerializedStyles = css`
   flex-wrap: wrap;
   justify-content: space-between;
   list-style: none;
-  padding: 30px;
+  padding: 5rem 0;
 `;
 
 const Pagination = ({ previous, next }: Props) => (
   <ul css={ulStyles}>
     <li>
       {previous && previous.title && (
-        <Link to={`/${previous.slug}`} rel="prev">
+        <Link css={link} to={`/${previous.slug}`} rel="prev">
           ← &nbsp; Prev
         </Link>
       )}
     </li>
     <li>
       {next && next.title && (
-        <Link to={`/${next.slug}`} rel="next">
+        <Link css={link} to={`/${next.slug}`} rel="next">
           Next &nbsp; →
         </Link>
       )}
