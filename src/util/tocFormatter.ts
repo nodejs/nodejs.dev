df0abde7ded20527aedcfbd73437a0a3434c4e40
Kevin J <qaz111id@gmail.com>
2019-04-10 03:51:21 -0700

    Fix TOC links for HTML code tags (#234)
    * fix: Fix code tags in TOC links
    
    * style: Use prettier formatting
    
    * style: Code format function titles
    
    * fix: Make util function more safe
    
    Co-Authored-By: kevjin <qaz111id@gmail.com>
    
    * style: Run prettier

---
+++ src/util/tocFormatter.ts
@@ -0,0 +1,6 @@
+// See related issue: https://github.com/gatsbyjs/gatsby/issues/8982
+export function fixTocCodeTag(tocLinks: string = '') {
+  const lessThanSignRegex = /&#x3C;/gi;
+
+  return tocLinks.replace(lessThanSignRegex, '<');
+}
