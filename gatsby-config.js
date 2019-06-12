7d83be73604d2923ba91f10c2db8474b91f862ae
Rich Trott <rtrott@gmail.com>
2019-06-12 15:21:02 -0700

    chore: Upgrade dependencies
    Upgrade dependencies in .github/action/result-poster/package.json to
    quell several (but not all) `npm audit` warnings.
    
    (gatsby-config.js needed prettier run on it, so that's why it's
    modified. Guess someone committed something while bypassing the prettier
    checks?)

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,9 +115,7 @@ module.exports = {
           });
           allMarkdownRemark.edges.map(edge => {
             pages.push({
-              url: `${site.siteMetadata.siteUrlNoSlash}/${
-                edge.node.fields.slug
-              }`,
+              url: `${site.siteMetadata.siteUrlNoSlash}/${edge.node.fields.slug}`,
               changefreq: `daily`,
               priority: 0.7,
             });
