ba6834c3978c798c7e2b22e634b37abf4c65ec1c
Jon Church <me@jonchurch.com>
2019-09-07 16:20:14 -0400

    feat: Use useReleaseHistory hook to fetch data

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useReleaseHistory } from '../hooks'
 import Hero from '../components/hero';
 import Layout from '../components/layout';
 
 export default () => {
+  const releaseHistory = useReleaseHistory().slice(0, 24)
   const title = 'Download Node.js';
   const description = 'Come get me!';
 
@@ -11,6 +13,7 @@ export default () => {
       <Hero title={title} />
       <article style={{ width: '100%' }} className="article-reader">
         <p>Welcome to the Downloads Page!</p>
+        <p>{JSON.stringify(releaseHistory)}</p>
       </article>
     </Layout>
   );
