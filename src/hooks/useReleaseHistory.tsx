79316f7b236da8db0fef7d9e53fcefd5133e2183
Adam Miller <adam@pages.me>
2019-09-07 12:56:42 -0700

    feat: Add API docs hook and render list of modules for POC API page

---
+++ src/hooks/useReleaseHistory.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+
+export function useReleaseHistory() {
+  const releasesURL = 'https://nodejs.org/dist/index.json';
+  const [releaseHistory, setReleaseHistory] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(releasesURL).then(data => data.json());
+      setReleaseHistory(result);
+    };
+    fetchData();
+  }, []);
+
+  return releaseHistory;
+}
