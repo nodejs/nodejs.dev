2544d61a02e5fa0b41973be72473531c140cea43
Adam Miller <adam@pages.me>
2019-09-07 16:59:33 -0700

    feat: API Docs Version Switcher (#310)
    * feat: API Docs preview and version switcher
    * feat: Add design tokens and style-guide page (#311)
    * feat: API Docs preview and version switcher
    * feat: API Page Types
    * feat: Light/dark toggle in nav

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import 'prismjs/themes/prism-okaidia.css';
 import React, { useEffect, useRef } from 'react';
 import Header from './header';
-import '../styles/_tokens.css';
+import '../styles/tokens.css';
 import '../styles/layout.css';
 import '../styles/mobile.css';
 import SEO from './seo';
--- src/hooks/useApiDocs.tsx
+++ src/hooks/useApiDocs.tsx
@@ -1,17 +1,70 @@
 import { useState, useEffect } from 'react';
-
-// TODO: Flesh out API types.
-/* tslint:disable */
-interface APIResponse {
-  classes: any[];
-  globals: any[];
-  methods: any[];
-  miscs: any[];
-  modules: any[];
+export interface ApiDocsMeta {
+  added: string[];
+  changes: string[];
 }
-/* tslint:enable */
+export interface ApiDocsBase {
+  type: string;
+  name: string;
+  textRaw: string;
+  displayName?: string;
+  desc?: string;
+  meta?: ApiDocsMeta;
+  introduced_in?: string;
+}
 
-export function useApiData(version: string): APIResponse {
+export interface ApiDocsMiscs extends ApiDocsBase {
+  type: 'misc';
+  miscs?: ApiDocsMiscs[];
+}
+
+export interface ApiDocsMethod extends ApiDocsBase {
+  type: 'method';
+  signatures: ApiDocsSignature[];
+}
+export interface ApiDocsSignature extends ApiDocsBase {
+  params: ApiDocsProp[];
+}
+
+export interface ApiDocsClass extends ApiDocsBase {
+  type: 'class';
+  methods: ApiDocsMethod[];
+  properties: ApiDocsProp[];
+  signatures: ApiDocsSignature[];
+}
+
+export interface ApiDocsProp extends ApiDocsBase {
+  type: string;
+}
+
+export interface ApiDocsModule extends ApiDocsBase {
+  type: 'module';
+  stability: number;
+  stabilityText: string;
+  modules?: ApiDocsModule[];
+  methods?: ApiDocsMethod[];
+  classes?: ApiDocsClass[];
+  properties?: ApiDocsProp[];
+  miscs?: ApiDocsMiscs[];
+}
+
+export interface APIResponse {
+  classes: ApiDocsClass[];
+  globals: ApiDocsProp[];
+  methods: ApiDocsMethod[];
+  miscs: ApiDocsMiscs[];
+  modules: ApiDocsModule[];
+}
+
+export type ApiDocsObj =
+  | ApiDocsMiscs
+  | ApiDocsMethod
+  | ApiDocsSignature
+  | ApiDocsClass
+  | ApiDocsProp
+  | ApiDocsModule;
+
+export function useApiData(version: string | null): APIResponse {
   const [apiData, setApiData] = useState<APIResponse>({
     classes: [],
     globals: [],
@@ -27,8 +80,10 @@ export function useApiData(version: string): APIResponse {
       );
       setApiData((await res.json()) as APIResponse);
     };
-    fetchData();
-  }, []);
+    if (version) {
+      fetchData();
+    }
+  }, [version]);
 
   return apiData;
 }
