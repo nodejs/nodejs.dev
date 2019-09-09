6a9036776a528b50703eeff984e11f7a53dadf7b
Jon Church <me@jonchurch.com>
2019-09-08 23:12:58 -0400

    chore: Fix eslint errors in js files

--- test/__mocks__/gatsby.js
+++ test/__mocks__/gatsby.js
@@ -1,14 +1,13 @@
 const React = require('react');
+
 const gatsby = jest.requireActual('gatsby');
 
 module.exports = {
   ...gatsby,
   graphql: jest.fn(),
-  Link: jest.fn().mockImplementation(({ to, ...rest }) =>
-    React.createElement('a', {
+  Link: jest.fn().mockImplementation(({ to, ...rest }) => React.createElement('a', {
       ...rest,
       href: to,
-    })
-  ),
+    })),
   StaticQuery: jest.fn(),
 };
