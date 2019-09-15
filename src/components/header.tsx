cddcad22e5171f5ed0d2a53d340e56211fd08c08
Oscar G <ogonzal87@gmail.com>
2019-09-14 19:19:04 -0700

    feat: Added home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,47 +1,45 @@
 import { Link } from 'gatsby';
 import React from 'react';
-import { css, SerializedStyles } from '@emotion/core';
+import logoLight from '../images/logos/nodejs-logo-light-mode.svg';
+import logoDark from '../images/logos/nodejs-logo-dark-mode.svg';
 
-/* eslint-disable-next-line @typescript-eslint/no-var-requires */
-const logo = require('../images/logo.svg');
-
-const ulStyles: SerializedStyles = css`
-  @media (max-width: 380px) {
-    padding: 0;
-  }
-  margin: 0 auto;
-  padding: 0 4.8rem;
-  display: flex;
-  align-items: center;
-  list-style: none;
-`;
-
-const Header = (): JSX.Element => (
+const Header = () => (
   <nav className="nav">
-    <ul css={ulStyles}>
-      <li>
-        <Link to="/" style={{ display: 'block' }}>
-          <img src={logo} alt="Node.js" className="nav__logo" />
-        </Link>
-      </li>
+    <div className="logo">
+      <Link to="/">
+        <img
+          src={logoLight}
+          alt="Node.js"
+          className="nav__logo light-mode-only"
+        />
+        <img
+          src={logoDark}
+          alt="Node.js"
+          className="nav__logo dark-mode-only"
+        />
+      </Link>
+    </div>
+
+    <ul className="nav__tabs__container">
       <li className="nav__tabs">
         <Link to="/learn">Learn</Link>
       </li>
       <li className="nav__tabs">
-        <Link to="/docs">Docs</Link>
-      </li>
-      <li className="nav__tabs">
-        <Link to="/community">Community</Link>
+        <Link to="/docs">Documentation</Link>
       </li>
       <li className="nav__tabs">
         <Link to="/download">Download</Link>
       </li>
-      <li style={{ flexGrow: 1 }} />
+    </ul>
+
+    <div style={{ flexGrow: 1 }}></div>
+
+    <ul className="right-container">
       <li className="nav__tabs nav__tabs--right">
         <button
           type="button"
           className="dark-mode-toggle"
-          onClick={(): boolean => document.body.classList.toggle('dark-mode')}
+          onClick={() => document.body.classList.toggle('dark-mode')}
         >
           <span className="sr-only">Toggle Dark Mode</span>
           <i className="material-icons light-mode-only">nights_stay</i>
@@ -52,13 +50,13 @@ const Header = (): JSX.Element => (
       <li className="nav__tabs">
         <a
           target="_blank"
-          rel="noreferrer noopener"
           href="https://github.com/nodejs/nodejs.dev"
+          rel="noopener noreferrer"
         >
           <span className="sr-only">GitHub</span>
           <svg
-            width="2.0rem"
-            height="2.0rem"
+            width="2.4rem"
+            height="2.4rem"
             viewBox="0 0 438.549 438.549"
             style={{ fill: 'var(--color-text-accent)' }}
           >
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,121 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Hero from '../components/hero';
 import Layout from '../components/layout';
-
 import '../util/konami';
 
-export default function HomePage(): JSX.Element {
-  const title = 'Home Page';
+import '../styles/index-page.css';
+
+import featureImg from '../images/placeholder-img.svg';
+
+import logoImg1 from '../images/logos/ibm-logo.svg';
+import logoImg2 from '../images/logos/linkedin-logo.svg';
+import logoImg3 from '../images/logos/microsoft-logo.svg';
+import logoImg4 from '../images/logos/netflix-logo.svg';
+import logoImg5 from '../images/logos/paypal-logo.svg';
+
+import GetStartedIllustration1 from '../images/illustrations/beginners-guide-illustration.svg';
+import GetStartedIllustration2 from '../images/illustrations/do-more-illustration.svg';
+
+const nodeFeature1 =
+  'Lorem ipsum dolor amet pug vape +1 poke pour-over kitsch tacos meh. ';
+const nodeFeature2 =
+  'Lorem ipsum dolor amet pug vape +1 poke pour-over kitsch tacos meh. ';
+const nodeFeature3 =
+  'Lorem ipsum dolor amet pug vape +1 poke pour-over kitsch tacos meh. ';
+
+const NodeFeature = ({ img, featureText }: Props) => {
+  return (
+    <div className="node-features__feature">
+      <img src={img} alt="node feature" />
+      <p className="t-caption">{featureText}</p>
+    </div>
+  );
+};
+
+export default function Index(): JSX.Element {
+  const title = 'The power of JavaScript minus the browser';
+  const subTitle =
+    'Node.js is a free, open-sourced, cross-platform JavaScript run-time environment that lets developers write command line tools and server-side scripts outside of a browser.';
   const description = 'Welcome to Node.js!';
 
   return (
     <Layout title={title} description={description}>
-      <Hero title={title} />
-      <article style={{ width: '100%' }} className="article-reader">
-        <p>Welcome to the Home Page!</p>
-      </article>
+      <div className="home-page">
+        <Hero title={title} subTitle={subTitle} />
+        <section className="node-demo"></section>
+
+        <section className="node-features">
+          <NodeFeature img={featureImg} featureText={nodeFeature1} />
+          <NodeFeature img={featureImg} featureText={nodeFeature2} />
+          <NodeFeature img={featureImg} featureText={nodeFeature3} />
+        </section>
+
+        <section className="join-node">
+          <h4 className="t-headline">
+            <span className="accent">Join</span> the community
+          </h4>
+          <div className="join-node-form-container">
+            <p className="t-body2">
+              We’ll never share your information and always respect your inbox -
+              quality content only, we promise.{' '}
+            </p>
+            <div style={{ display: `flex`, alignItems: 'flex-start' }}>
+              <input
+                type="email"
+                placeholder="node@nodejs.dev"
+                className="input-subscribe"
+              />
+              <button className="btn-subscribe t-body1" type="button">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </section>
+
+        <section className="trusted-by">
+          <h4 className="t-headline">
+            Trusted by development teams around the world
+          </h4>
+          <p>Including IBM, LinkedIn, Microsoft, Netflix, and PayPal.</p>
+          <div className="logos-container">
+            <img src={logoImg1} alt="ibm logo" />
+            <img src={logoImg2} alt="linkedin logo" />
+            <img src={logoImg3} alt="microsoft logo" />
+            <img src={logoImg4} alt="netflix logo" />
+            <img src={logoImg5} alt="paypal logo" />
+          </div>
+        </section>
+
+        <section className="get-started-callouts">
+          <Link to="/learn" className="get-started-callout">
+            <img src={GetStartedIllustration1} alt="" />
+            <h5 className="t-headline">A beginner’s guide</h5>
+            <p>
+              Lorem ipsum dolor amet pug vape +1 poke pour-over kitsch tacos
+              meh.
+            </p>
+          </Link>
+
+          <Link to="/documentation" className="get-started-callout">
+            <img src={GetStartedIllustration2} alt="" />
+            <h5 className="t-headline">Do even more with Node</h5>
+            <p>
+              Lorem ipsum dolor amet pug vape +1 poke pour-over kitsch tacos
+              meh.
+            </p>
+          </Link>
+        </section>
+
+        <Link to="/learn" className="btn-primary">
+          Get Started
+        </Link>
+      </div>
     </Layout>
   );
+}
+
+interface Props {
+  img: string;
+  featureText: string;
 }
