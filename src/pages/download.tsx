bbf5816f4a07ec069254821666548bc2c86c95d2
Jon Church <me@jonchurch.com>
2019-09-16 15:13:54 -0400

    feat: Create and use detectOS util function

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -6,6 +6,10 @@ import ReleaseTable from '../components/release-table';
 import ReleaseToggle from '../components/release-toggle';
 import ReleaseCards from '../components/release-cards';
 
+import { detectOS } from '../util/detectOS';
+
+const userOS = detectOS();
+
 export default function DownloadPage(): JSX.Element {
   const releaseHistory = useReleaseHistory().slice(0, 50);
   const [ltsSelected, setLtsSelected] = useState(true);
@@ -27,6 +31,7 @@ export default function DownloadPage(): JSX.Element {
           Download the Node.js source code, a pre-built installer for your
           platform, or install via package manager.
         </p>
+        <p>You are currently on a {userOS} machine!</p>
         <ReleaseToggle selected={ltsSelected} onToggle={setLtsSelected} />
         <ReleaseCards line={selectedLine} />
         <ReleaseTable releases={releaseHistory} />
---
+++ src/util/detectOS.ts
@@ -0,0 +1,19 @@
+export enum UserOS {
+  MAC = 'MAC',
+  WIN = 'WIN',
+  UNIX = 'UNIX',
+  LINUX = 'LINUX',
+  MOBILE = 'MOBILE',
+  UNKNOWN = 'UNKNOWN',
+}
+
+export function detectOS(): UserOS {
+  let OS = UserOS.UNKNOWN;
+  // could be a switch statement? prolly not necessary
+  if (navigator.appVersion.indexOf('Win') !== -1) OS = UserOS.WIN;
+  if (navigator.appVersion.indexOf('Mac') !== -1) OS = UserOS.MAC;
+  if (navigator.appVersion.indexOf('X11') !== -1) OS = UserOS.UNIX;
+  if (navigator.appVersion.indexOf('Linux') !== -1) OS = UserOS.LINUX;
+  // not currently checking for mobile devices
+  return OS;
+}
