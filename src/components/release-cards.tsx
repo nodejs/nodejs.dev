89943dad03cd1d7754e8990c061d5d991ed7cf4a
Jon Church <me@jonchurch.com>
2019-09-16 10:45:38 -0400

    feat: Create ReleaseCards component

---
+++ src/components/release-cards.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ReleaseData } from '../hooks/useReleaseHistory';
+
+interface Props {
+  line?: ReleaseData;
+}
+
+export default function ReleaseCards({ line }: Props): JSX.Element {
+  const fileName = line && line.version;
+  return (
+    <div>
+      <div>
+        <p>Windows Installer</p>
+        <a
+          href={`https://nodejs.org/dist/${fileName}/node-${line &&
+            line.version}-x86.msi`}
+        >
+          node-{line && line.version}.x86.msi
+        </a>
+      </div>
+      <div>
+        <p>Mac Installer</p>
+        <a
+          href={`https://nodejs.org/dist/${fileName}/node-${line &&
+            line.version}.pkg`}
+        >
+          node-{line && line.version}.pkg
+        </a>
+      </div>
+      <div>
+        <p>Source Code</p>
+        <a
+          href={`https://nodejs.org/dist/${fileName}/node-${line &&
+            line.version}.tar.gz`}
+        >
+          node-{line && line.version}.tar.gz
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -4,17 +4,20 @@ import Hero from '../components/hero';
 import Layout from '../components/layout';
 import ReleaseTable from '../components/release-table';
 import ReleaseToggle from '../components/release-toggle';
+import ReleaseCards from '../components/release-cards';
 
 export default function DownloadPage(): JSX.Element {
-  const releaseHistory = useReleaseHistory().slice(0, 25);
+  const releaseHistory = useReleaseHistory().slice(0, 50);
   const [ltsSelected, setLtsSelected] = useState(true);
   const title = 'Download Node.js';
   const description = 'Come get me!';
 
   const lts = releaseHistory.find((release): boolean => release && release.lts);
   const current = releaseHistory.find(
-    (release): boolean => release && !!release.lts
+    (release): boolean => release && !release.lts
   );
+
+  const selectedLine = ltsSelected ? lts : current;
 
   return (
     <Layout title={title} description={description}>
@@ -25,6 +28,7 @@ export default function DownloadPage(): JSX.Element {
           platform, or install via package manager.
         </p>
         <ReleaseToggle selected={ltsSelected} onToggle={setLtsSelected} />
+        <ReleaseCards line={selectedLine} />
         <ReleaseTable releases={releaseHistory} />
       </article>
     </Layout>
