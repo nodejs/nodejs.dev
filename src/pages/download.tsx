274f049202efcd621b8ddbc54ab051eb0b1f4211
Jon Church <me@jonchurch.com>
2019-09-16 10:11:08 -0400

    feat: Create ReleaseToggle component

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { useReleaseHistory } from '../hooks';
+import React, { useState } from 'react';
+import { useReleaseHistory } from '../hooks/useReleaseHistory';
 import Hero from '../components/hero';
 import Layout from '../components/layout';
 import ReleaseTable from '../components/release-table';
+import ReleaseToggle from '../components/release-toggle';
 
 export default function DownloadPage(): JSX.Element {
   const releaseHistory = useReleaseHistory().slice(0, 25);
+  const [ltsSelected, setLtsSelected] = useState(true);
   const title = 'Download Node.js';
   const description = 'Come get me!';
+
+  const lts = releaseHistory.find((release): boolean => release && release.lts);
+  const current = releaseHistory.find(
+    (release): boolean => release && !!release.lts
+  );
 
   return (
     <Layout title={title} description={description}>
       <Hero title={title} />
       <article style={{ width: '100%' }} className="article-reader">
-        <p>Welcome to the Downloads Page!</p>
+        <p>
+          Download the Node.js source code, a pre-built installer for your
+          platform, or install via package manager.
+        </p>
+        <ReleaseToggle selected={ltsSelected} onToggle={setLtsSelected} />
         <ReleaseTable releases={releaseHistory} />
       </article>
     </Layout>
