993dbce16900e74455a0f6b7142baf87ad1616d8
Saleh Abdel Motaal <saleh@smotaal.io>
2019-11-10 07:25:40 -0500

    build: Upgrade to @smotaal.io/dark-mode-controller@0.0.3

--- src/util/darkModeController.ts
+++ src/util/darkModeController.ts
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-import { DarkModeController } from '@smotaal.io/dark-mode-controller';
+import { DarkModeController } from '@smotaal.io/dark-mode-controller/commonjs';
 
 interface ReactDarkModeController extends DarkModeController {
   onPointerDown(
@@ -10,6 +10,9 @@ interface ReactDarkModeController extends DarkModeController {
   ): void;
   handleEvent(event?: React.PointerEvent | React.MouseEvent | Event): void;
 }
+
+// TODO: Figure out a way around HRM glitching (tackled upstream)
+// SEE: https://github.com/SMotaal/meta/issues/7
 
 // We're exporting a singleton
 export default new DarkModeController() as ReactDarkModeController;
