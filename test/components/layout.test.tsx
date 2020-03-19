b15fe49ed53b63daada1f1d9d605434341bd66ae
Onur Laru <onur.saygi@seidenader.de>
2020-03-19 15:43:05 +0100

    test: Refactor layout tests with mocks

--- test/components/layout.test.tsx
+++ test/components/layout.test.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Layout from '../../src/components/layout';
+import { createLayoutData } from '../__fixtures__/layout';
 
 describe('Layout component', () => {
   it('renders correctly', () => {
-    const title = 'TEST TITLE';
-    const description = 'This is a discription.';
+    const layoutData = createLayoutData();
+    const { title, description, children, showFooter } = layoutData;
     const tree = renderer
       .create(
-        <Layout title={title} description={description} showFooter={true}>
-          <span>Test Content</span>
+        <Layout title={title} description={description} showFooter={showFooter}>
+          {children}
         </Layout>
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
   it('renders correctly without footer', () => {
-    const title = 'TEST TITLE';
-    const description = 'This is a discription.';
+    const layoutData = createLayoutData();
+    const { title, description, children, showFooter } = layoutData;
     const tree = renderer
       .create(
         <Layout title={title} description={description} showFooter={false}>
-          <span>Test Content</span>
+          {children}
         </Layout>
       )
       .toJSON();
