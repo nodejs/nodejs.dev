3366fdf47474bb14e4945932e9ce64ed0d1c2051
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-03-21 05:04:42 -0300

    test(article.tsx): Add scroll event and set header id while scrolling

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable */
+/* tslint:disable */
+
 import React from 'react';
 import { PaginationInfo } from '../types';
 import AuthorsList from './authors-list';
@@ -23,16 +26,86 @@ const Article = ({
   next,
   relativePath,
   authors,
-}: Props): JSX.Element => (
-  <article className="article-reader">
-    <h1 className="article-reader__headline">{title}</h1>
-    <TOC heading="TABLE OF CONTENTS" tableOfContents={tableOfContents} />
-    {/* eslint-disable-next-line react/no-danger */}
-    <div dangerouslySetInnerHTML={{ __html: html }} />
-    <AuthorsList authors={authors} />
-    <EditLink relativePath={relativePath} />
-    <Pagination previous={previous} next={next} />
-  </article>
-);
+}: Props): JSX.Element => {
+  const element = React.useRef<HTMLElement | null>(null);
+  const [headers, setHeaders] = React.useState<{ id: string; scrollYPos: number; }[]>([]);
+
+  const handleRef = (ref?: HTMLElement | null): void => {
+    if (ref) {
+      element.current = ref;
+    }
+  };
+
+  React.useEffect(() => {
+    if (window.location.hash) {
+      const el = document.getElementById(window.location.hash.substr(1));
+
+      if (el) {
+        el.scrollIntoView({
+          behavior: 'smooth',
+        });
+      }
+    }
+
+    if (element.current) {
+      // save current headers positions
+      const newHeaders = Array.from(element.current.getElementsByTagName('h2')).map((header) => {
+        console.log('header', header);
+
+        return {
+          id: `#${header.getAttribute('id')}`,
+          scrollYPos: (header.getBoundingClientRect().top) - 64,
+        };
+      });
+
+      setHeaders(newHeaders);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    let debounce: any;
+
+    const handleScroll = () => {
+      if (debounce) {
+        clearTimeout(debounce);
+      }
+
+      debounce = setTimeout(() => {
+        // find right header based on current position
+        const el = headers.find((header, index) => {
+          if (headers[index + 1]) {
+            return window.scrollY >= header.scrollYPos
+              && window.scrollY < headers[index + 1].scrollYPos;
+          }
+
+          return window.scrollY >= header.scrollYPos;
+        });
+
+        // set header id in url as hash
+        if (el) {
+          window.history.replaceState(null, '', el.id);
+        }
+      }, 10);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [headers]);
+
+  return (
+    <article className="article-reader">
+      <h1 className="article-reader__headline">{title}</h1>
+      <TOC heading="TABLE OF CONTENTS" tableOfContents={tableOfContents} />
+      {/* eslint-disable-next-line react/no-danger */}
+      <div ref={handleRef} dangerouslySetInnerHTML={{ __html: html }} />
+      <AuthorsList authors={authors} />
+      <EditLink relativePath={relativePath} />
+      <Pagination previous={previous} next={next} />
+    </article>
+  );
+};
 
 export default Article;
