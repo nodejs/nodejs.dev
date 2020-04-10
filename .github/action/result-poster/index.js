aad5263a06859f201f793cd67ad4514dd90f1b2c
Nick Schonning <nschonni@gmail.com>
2020-04-09 22:26:57 -0400

    build: Add CI for Result Poster Action (#534)
    * build: Add CI for Result Poster Action
    
    * fix: linting on result-poster JS
    
    * fix: Remove call to missing file

--- .github/action/result-poster/index.js
+++ .github/action/result-poster/index.js
@@ -1,14 +1,14 @@
 const { Toolkit } = require('actions-toolkit')
 const { context, github } = new Toolkit({ event: 'check_suite' })
 
-const name = context.payload.check_suite.app.name;
+const name = context.payload.check_suite.app.name
 const conclusion = context.payload.check_suite.conclusion
-const sha = context.payload.check_suite.head_sha;
-const prs = context.payload.check_suite.pull_requests;
+const sha = context.payload.check_suite.head_sha
+const prs = context.payload.check_suite.pull_requests
 
 if (name === 'Google Cloud Build' && conclusion === 'success' && prs[0]) {
   github.issues.createComment(context.repo({
     number: prs[0].number,
-    body: `Preview at: https://storage.googleapis.com/staging.nodejs.dev/${sha.slice(0,7)}/index.html`
-  }));
+    body: `Preview at: https://storage.googleapis.com/staging.nodejs.dev/${sha.slice(0, 7)}/index.html`
+  }))
 }
