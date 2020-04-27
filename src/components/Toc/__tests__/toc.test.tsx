13aee791aafbf58b7e04bd46a2c3f377ac997f00
Ben Halverson <benhalverson33@gmail.com>
2020-04-27 01:25:39 -0700

    Fixed spelling and lowercase issue

---
+++ src/components/Toc/__tests__/toc.test.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import TOC from '..';
+
+describe('TOC component', () => {
+  it('renders correctly', () => {
+    const heading = 'TABLE OF CONTENTS';
+    const tableOfContents = 'mock-toc';
+    const tree = renderer
+      .create(<TOC heading={heading} tableOfContents={tableOfContents} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('does not render without a table of contents', () => {
+    const heading = 'TABLE OF CONTENTS';
+    const tableOfContents = null;
+    const tree = renderer.create(
+      <TOC heading={heading} tableOfContents={tableOfContents} />
+    );
+    expect(tree.getInstance()).toBeNull();
+  });
+});
