4c787eeed7e651c8006556cc84866e2e662e8667
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-04-30 12:07:59 -0400

    fix(hero.tsx): Removed hardcoded version and use useReleaseHistory

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import { useReleaseHistory, ReleaseData } from '../hooks/useReleaseHistory';
+
 interface Props {
   title: string;
   subTitle: string;
 }
 
-const NodeVersion = 'Version 10.15.3';
+const Hero = ({ title, subTitle }: Props): JSX.Element => {
+  // find first lts version (first found is last LTS)
+  const lastLts = useReleaseHistory().find(
+    (release: ReleaseData): boolean => !!release.lts
+  );
 
-const Hero = ({ title, subTitle }: Props): JSX.Element => (
-  <div className="home-page-hero">
-    <h1>{title}</h1>
-    <h2 className="sub-title t-subheading">{subTitle}</h2>
-    <div className="btn-ctas">
-      <div className="download-lts-container">
-        <button className="download-lts-cta t-body1" type="button">
-          Download Node (LTS)
-        </button>
-        <p className="links t-caption">
-          {NodeVersion} - <Link to="/download">What’s new</Link> /{' '}
-          <Link to="/download">Get Current</Link>
-        </p>
+  return (
+    <div className="home-page-hero">
+      <h1>{title}</h1>
+      <h2 className="sub-title t-subheading">{subTitle}</h2>
+      <div className="btn-ctas">
+        <div className="download-lts-container">
+          <button className="download-lts-cta t-body1" type="button">
+            Download Node (LTS)
+          </button>
+          <p className="links t-caption">
+            {lastLts ? `Version ${lastLts.version.substr(1)} - ` : ''}
+            <Link to="/download">What’s new</Link> /{' '}
+            <Link to="/download">Get Current</Link>
+          </p>
+        </div>
+        <Link to="/learn">
+          <button className="learn-cta t-body1" type="button">
+            Learn Node
+          </button>
+        </Link>
       </div>
-      <Link to="/learn">
-        <button className="learn-cta t-body1" type="button">
-          Learn Node
-        </button>
-      </Link>
     </div>
-  </div>
-);
+  );
+};
 
 export default Hero;
