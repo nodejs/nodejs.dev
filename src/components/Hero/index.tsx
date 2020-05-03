e8719d2b1e33fae8722b3ccb4bb978805a124b09
Giancarlo Otárola <g.otarolarobledo@gmail.com>
2020-05-02 23:16:13 -0400

    Homepage: Changes hardcoded data version and download urls (#665)
    * fix(hero.tsx): Removed hardcoded version and use useReleaseHistory
    
    * fix(hero.tsx): Get last change from staging and fix hooks path
    
    * fix(hero.tsx): Update hero.tsx snap
    
    * fix(hero.tsx): Remove whats new link and fix lts/current download urls
    
    * fix(doc.tsx): Fix lint with npm run lint -- --fix

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,34 +1,83 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import { detectOS, UserOS } from '../../util/detectOS';
+import { useReleaseHistory, ReleaseData } from '../../hooks/useReleaseHistory';
+
 interface Props {
   title: string;
   subTitle: string;
 }
 
-const NodeVersion = 'Version 10.15.3';
+function downloadUrlByOS(userOS: UserOS, version: string): string {
+  const baseURL = `https://nodejs.org/dist/${version}`;
 
-const Hero = ({ title, subTitle }: Props): JSX.Element => (
-  <div className="home-page-hero">
-    <h1>{title}</h1>
-    <h2 className="sub-title t-subheading">{subTitle}</h2>
-    <div className="btn-ctas">
-      <div className="download-lts-container">
-        <button className="download-lts-cta t-body1" type="button">
-          Download Node (LTS)
-        </button>
-        <p className="links t-caption">
-          {NodeVersion} - <Link to="/download">What’s new</Link> /{' '}
-          <Link to="/download">Get Current</Link>
-        </p>
+  if (userOS === UserOS.MOBILE) {
+    return baseURL;
+  }
+
+  if (userOS === UserOS.MAC) {
+    return `${baseURL}/node-${version}.pkg`;
+  }
+
+  if (userOS === UserOS.WIN) {
+    if (
+      navigator.appVersion.indexOf('WOW64') !== -1 ||
+      navigator.appVersion.indexOf('Win64') !== -1
+    ) {
+      return `${baseURL}/node-${version}-x64.msi`;
+    }
+
+    return `${baseURL}/node-${version}-x86.msi`;
+  }
+
+  return `${baseURL}/node-${version}.tar.gz`;
+}
+
+const Hero = ({ title, subTitle }: Props): JSX.Element => {
+  const userOS = detectOS();
+  const [currentRelease, ...releases] = useReleaseHistory();
+
+  // find first lts version (first found is last LTS)
+  const lastLTSRelease = releases.find(
+    (release: ReleaseData): boolean => release.lts
+  );
+
+  const ltsVersionUrl = downloadUrlByOS(
+    userOS,
+    lastLTSRelease ? lastLTSRelease.version : ''
+  );
+  const currentVersionUrl = downloadUrlByOS(
+    userOS,
+    currentRelease ? currentRelease.version : ''
+  );
+
+  return (
+    <div className="home-page-hero">
+      <h1>{title}</h1>
+      <h2 className="sub-title t-subheading">{subTitle}</h2>
+      <div className="btn-ctas">
+        <div className="download-lts-container">
+          <a href={ltsVersionUrl}>
+            <button className="download-lts-cta t-body1" type="button">
+              Download Node (LTS)
+            </button>
+          </a>
+          <p className="links t-caption">
+            {lastLTSRelease
+              ? `Version ${lastLTSRelease.version.substr(1)} - `
+              : ''}
+            <a href={currentVersionUrl}>Get Current</a>
+          </p>
+        </div>
+        <Link to="/learn">
+          <button className="learn-cta t-body1" type="button">
+            Learn Node
+          </button>
+        </Link>
       </div>
-      <Link to="/learn">
-        <button className="learn-cta t-body1" type="button">
-          Learn Node
-        </button>
-      </Link>
     </div>
-  </div>
-);
+  );
+};
 
 export default Hero;
