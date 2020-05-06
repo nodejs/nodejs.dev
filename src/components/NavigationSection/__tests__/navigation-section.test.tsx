cac05bcfcc300fba6e53599d9dfc113672b902ba
tstreamDOTh <tstream.h@outlook.com>
2020-05-06 12:03:27 +0530

    test: Navigation, NavigationItem, NavigationSection

---
+++ src/components/NavigationSection/__tests__/navigation-section.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import NavigationSection from '..';
+
+describe('NavigationSection component', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <NavigationSection
+          key="123"
+          title="Introduction"
+          section={[
+            {
+              slug: 'intro',
+              title: 'Section 1',
+              section: '...',
+            },
+          ]}
+          currentSlug=""
+          onItemClick={console.log}
+          readSections={new Set()}
+          autoScroll={console.log}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
