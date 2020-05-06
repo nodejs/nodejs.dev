64553e3c052ee2c3aa1130a3e84b1ee29b6f175c
Ben Halverson <benhalverson33@gmail.com>
2020-05-06 01:45:18 -0700

    refactor(homepage): Bringing back the logos to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,11 @@ import leafsIllustrationFront from '../images/illustrations/leafs-front.svg';
 import leafsIllustrationMiddle from '../images/illustrations/leafs-middle.svg';
 import leafsIllustrationBack from '../images/illustrations/leafs-back.svg';
 import dotsIllustration from '../images/illustrations/dots.svg';
+import logoImg1 from '../images/logos/ibm-logo.svg';
+import logoImg2 from '../images/logos/linkedin-logo.svg';
+import logoImg3 from '../images/logos/microsoft-logo.svg';
+import logoImg4 from '../images/logos/netflix-logo.svg';
+import logoImg5 from '../images/logos/paypal-logo.svg';
 
 export default function Index(): JSX.Element {
   const title = 'Run JavaScript Everywhere.';
@@ -28,6 +33,20 @@ export default function Index(): JSX.Element {
           <img className="leafs-middle" src={leafsIllustrationMiddle} alt="" />
           <img className="leafs-back" src={leafsIllustrationBack} alt="" />
           <img className="dots" src={dotsIllustration} alt="" />
+        </section>
+
+        <section className="trusted-by">
+          <h4 className="t-headline">
+            Trusted by development teams around the world
+          </h4>
+          <p>Including IBM, LinkedIn, Microsoft, Netflix, and PayPal.</p>
+          <div className="logos-container">
+            <img src={logoImg1} alt="ibm logo" />
+            <img src={logoImg2} alt="linkedin logo" />
+            <img src={logoImg3} alt="microsoft logo" />
+            <img src={logoImg4} alt="netflix logo" />
+            <img src={logoImg5} alt="paypal logo" />
+          </div>
         </section>
 
         <Link to="/learn" className="btn-primary">
