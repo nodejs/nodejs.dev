9999ab155ab88b7bde2e3f8d486318044cc44395
Ahmad Awais <me@AhmadAwais.com>
2020-05-20 06:35:09 +0500

    feat(SVG-Features): Add react-icons for svg feat's
    The feature images on the homepage were previously using some png images
    that didn't scale well to different resolutions. An issue was opened
    surrounding changing them to svg icons. However, the initial icons
    suggested weren't licensed appropriately. Discussion seems to have
    circled around a couple of differnt icons available to the `react-icons`
    package, so this PR puts some alternatively licensed icons into place.
    
    - Add the icons, and remove the old pngs
    - Shift to some different icons so that all our icon are sourced from
      libraries that use the MIT license
    - Add start of a decision record outlining the use of react-icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { graphql } from 'gatsby';
+import { IoLogoNodejs, IoMdGitPullRequest, IoMdRocket } from 'react-icons/io';
 
 import Hero from '../components/Hero';
 import Layout from '../components/Layout';
@@ -15,23 +16,47 @@ import leafsIllustrationMiddle from '../images/illustrations/leafs-middle.svg';
 import leafsIllustrationBack from '../images/illustrations/leafs-back.svg';
 import dotsIllustration from '../images/illustrations/dots.svg';
 import InstallTabs from '../components/InstallTabs';
-
-import featureImg1 from '../images/feature-img-1.png';
-import featureImg2 from '../images/feature-img-2.png';
-import featureImg3 from '../images/feature-img-3.png';
 import GetStartedSection from '../components/GetStartedSection';
 
+interface NodeFeatureProps {
+  icon?: ReactElement;
+  heading: string;
+  description: string;
+}
+
+const styled = (icon: ReactElement): ReactElement =>
+  React.cloneElement(icon, { alt: 'Node Feature', className: 'feature-icon' });
+
+const features = [
+  {
+    icon: styled(<IoLogoNodejs />),
+    heading: 'JavaScript',
+    description:
+      'Node.js provides support for the JavaScript programming language',
+  },
+  {
+    icon: styled(<IoMdGitPullRequest />),
+    heading: 'Open Source',
+    description:
+      'Node.js is open source and actively maintained by contributors all over the world',
+  },
+  {
+    icon: styled(<IoMdRocket />),
+    heading: 'Everywhere',
+    description: 'Node.js has been adapted to work in a wide variety of places',
+  },
+];
+
 const NodeFeature = ({
-  img,
-  featureText,
-  featureHeader,
-  featureAltText,
-}: Props): JSX.Element => {
+  icon,
+  heading,
+  description,
+}: NodeFeatureProps): JSX.Element => {
   return (
     <div className="node-features__feature">
-      <img src={img} alt={featureAltText} />
-      <h4>{featureHeader}</h4>
-      <p>{featureText}</p>
+      {icon && icon}
+      <h4>{heading}</h4>
+      <p>{description}</p>
     </div>
   );
 };
@@ -87,24 +112,14 @@ export default function Index({
         </section>
 
         <section className="node-features">
-          <NodeFeature
-            img={featureImg1}
-            featureText={nodeFeature1}
-            featureHeader={nodeFeatureHeader1}
-            featureAltText={nodeFeatureAltText}
-          />
-          <NodeFeature
-            img={featureImg2}
-            featureText={nodeFeature2}
-            featureHeader={nodeFeatureHeader2}
-            featureAltText={nodeFeatureAltText}
-          />
-          <NodeFeature
-            img={featureImg3}
-            featureText={nodeFeature3}
-            featureHeader={nodeFeatureHeader3}
-            featureAltText={nodeFeatureAltText}
-          />
+          {features.map(feature => (
+            <NodeFeature
+              key={feature.heading}
+              icon={feature.icon}
+              heading={feature.heading}
+              description={feature.description}
+            />
+          ))}
         </section>
 
         <GetStartedSection
@@ -118,14 +133,6 @@ export default function Index({
     </Layout>
   );
 }
-
-interface Props {
-  img: string;
-  featureText: string;
-  featureHeader: string;
-  featureAltText: string;
-}
-
 interface HomepageProps {
   data: HomepageData;
 }
