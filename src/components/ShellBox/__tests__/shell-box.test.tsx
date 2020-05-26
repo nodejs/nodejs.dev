a1db01c81a9940a486da0047174ac2ebf334e2d2
Giancarlo Otarola <g.otarolarobledo@gmail.com>
2020-05-25 22:07:20 -0400

    fix: requested changes

--- src/components/ShellBox/__tests__/shell-box.test.tsx
+++ src/components/ShellBox/__tests__/shell-box.test.tsx
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import ShellBox from '../index';
 
-describe('ShellBox component', () => {
-  it('renders correctly', () => {
+describe('ShellBox component', (): void => {
+  it('renders correctly', (): void => {
     const textToCopy = 'text to be copy';
     const tree = renderer.create(<ShellBox textToCopy={textToCopy} />).toJSON();
     expect(tree).toMatchSnapshot();
