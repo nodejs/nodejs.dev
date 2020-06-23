42a97df2fc20f449da5b2a74b3c1803404f385b6
Ben Halverson <benhalverson33@gmail.com>
2020-06-22 21:24:42 -0700

    refactor(installTabs): Removed debian tab from homepage
    Removing because nodesource is not an official source and apt-get from Canonical doesnt have the
    latest lts

--- src/components/InstallTabs/index.tsx
+++ src/components/InstallTabs/index.tsx
@@ -18,7 +18,6 @@ const InstallTabs = (): JSX.Element => {
         <Tab>nvm (Linux)</Tab>
         <Tab>nvm (macOS)</Tab>
         <Tab>Chocolatey (Windows)</Tab>
-        <Tab>apt-get (Debian)</Tab>
       </TabList>
 
       <TabPanel>
@@ -95,32 +94,6 @@ const InstallTabs = (): JSX.Element => {
             <a
               className="install__docs-button-text"
               href="https://nodejs.org/en/download/package-manager/#windows"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read documentation
-            </a>
-          </button>
-        </div>
-      </TabPanel>
-      <TabPanel>
-        <div>
-          <code className="install__text">
-            <div className="install__text__line">
-              <span className="install__text__no-select">$</span>curl -sL
-              https://deb.nodesource.com/setup_12.x | sudo -E bash -
-            </div>
-            <div className="install__text__line">
-              <span className="install__text__no-select">$</span>sudo apt-get
-              install nodejs
-            </div>
-          </code>
-          <br />
-          <br />
-          <button type="button" className="install__docs-button">
-            <a
-              className="install__docs-button-text"
-              href="https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions-enterprise-linux-fedora-and-snap-packages"
               target="_blank"
               rel="noopener noreferrer"
             >
