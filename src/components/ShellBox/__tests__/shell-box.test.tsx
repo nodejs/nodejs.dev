c40bedf6430e82225d8ffa81f41adacadcd3069d
Bhavin_Agg <bhavin.a@lucideustech.com>
2020-10-06 19:30:23 +0530

    test(ShellBox): Add and update unit test cases for ShellBox component

--- src/components/ShellBox/__tests__/shell-box.test.tsx
+++ src/components/ShellBox/__tests__/shell-box.test.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import ShellBox from '../index';
+
+Object.assign(navigator, {
+  clipboard: {
+    writeText: jest.fn(),
+  },
+});
+
+const navigatorClipboardSpy = jest.spyOn(navigator.clipboard, 'writeText');
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('ShellBox component', (): void => {
   it('renders correctly', (): void => {
     const textToCopy = 'text to be copy';
-    const tree = renderer.create(<ShellBox textToCopy={textToCopy} />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container } = render(
+      <ShellBox textToCopy={textToCopy}>mock-children-code</ShellBox>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders correctly', async () => {
+    const textToCopy = 'text to be copy';
+    const { getByText } = render(
+      <ShellBox textToCopy={textToCopy}>mock-children-code</ShellBox>
+    );
+
+    navigatorClipboardSpy.mockImplementationOnce(() => Promise.resolve());
+
+    const buttonEle = getByText('copy');
+    fireEvent.click(buttonEle);
+
+    await waitFor(() => getByText('copied'));
+
+    expect(getByText('done')).toBeInTheDocument();
+    expect(navigatorClipboardSpy).toHaveBeenCalledTimes(1);
+    expect(navigatorClipboardSpy).toHaveBeenCalledWith(textToCopy);
   });
 });
