19e1faf605a4d6974d1532797c3dabb9c1b2bbba
Bhavin_Agg <bhavin.a@lucideustech.com>
2020-10-06 15:21:38 +0530

    test(DownloadCards): Add more unit tests for DownloadsCard which handles onClick

--- src/components/DownloadCards/__tests__/download-cards.test.tsx
+++ src/components/DownloadCards/__tests__/download-cards.test.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { fireEvent, render } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
 import DownloadCards from '..';
+import { UserOS } from '../../../util/detectOS';
 
 describe('DownloadCards component', (): void => {
   it('renders correctly', (): void => {
-    const tree = renderer.create(<DownloadCards userOS="WIN" />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container } = render(<DownloadCards userOS={UserOS.WIN} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('check click handler on DownloadCards component', (): void => {
+    const { getAllByRole } = render(<DownloadCards userOS={UserOS.MAC} />);
+
+    const listEle = getAllByRole('presentation');
+
+    expect(listEle[0]).toHaveClass('download-card--inactive');
+
+    fireEvent.click(listEle[0]);
+
+    expect(listEle[0]).toHaveClass('download-card--active');
   });
 });
