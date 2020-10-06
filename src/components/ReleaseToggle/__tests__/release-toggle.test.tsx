07ad97d56c82f7dc24609cdd240abb102f679b82
Bhavin_Agg <bhavin.a@lucideustech.com>
2020-10-06 12:01:26 +0530

    test: Add unit tests for ReleaseToggle component

---
+++ src/components/ReleaseToggle/__tests__/release-toggle.test.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import ReleaseToggle from '..';
+
+/**
+ * We need to spy on `Math.random()` to prevent random generation of id
+ * inside `<input>` element in `ReleaseToggle`component.
+ */
+const mathRandomSpy = jest.spyOn(Math, 'random');
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('ReleaseToggle component', (): void => {
+  it('renders correctly when selected is true', (): void => {
+    const mockOnToggle = jest.fn();
+    mathRandomSpy.mockImplementation(() => 1);
+
+    const { container } = render(
+      <ReleaseToggle selected onToggle={mockOnToggle} />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders correctly when selected is false', (): void => {
+    const mockOnToggle = jest.fn();
+    mathRandomSpy.mockImplementation(() => 1);
+
+    const { container } = render(
+      <ReleaseToggle selected={false} onToggle={mockOnToggle} />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders correctly and onToggle is called when clicked', (): void => {
+    const mockOnToggle = jest.fn();
+    mathRandomSpy.mockImplementation(() => 1);
+
+    const { getByLabelText } = render(
+      <ReleaseToggle selected={false} onToggle={mockOnToggle} />
+    );
+    const inputEle = getByLabelText('current') as HTMLInputElement;
+
+    fireEvent.click(inputEle);
+
+    expect(mockOnToggle).toHaveBeenCalledTimes(1);
+    expect(mockOnToggle).toHaveBeenCalledWith(true);
+  });
+});
