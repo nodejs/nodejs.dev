e5e3e2ef2dc84ccc16c12a5bff9b90cb8d24b09d
Ben Halverson <benhalverson33@gmail.com>
2020-10-22 01:54:18 -0700

    feat(DownloadTable): Added new story for DownloadTable

---
+++ stories/DownloadTable.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  NodeReleaseData,
+  getStaticReleaseData,
+} from '../src/hooks/useReleaseHistory.tsx';
+
+import DownloadTable from '../src/components/DownloadReleases/DownloadTable';
+
+import '../src/components/DownloadReleases/DownloadTable.scss';
+
+interface Props {
+  releases: NodeReleaseData[];
+}
+export default {
+  title: 'DownloadTable',
+  component: DownloadTable,
+};
+
+const DownloadTableExample = ({ releases }: Props): JSX.Element => {
+  const releaseHistory = getStaticReleaseData().slice(0, 10);
+
+  return <DownloadTable releases={releaseHistory} />;
+};
+
+export const root = (): JSX.Element => <DownloadTableExample />;
