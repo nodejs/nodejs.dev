b901064dc115ae280e5b942001d2cd9fbb00cec3
Manish Kumar <manishprivet@protonmail.com>
2021-03-26 20:03:31 +0530

    Removed extra blogs and added a dummy blog

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -10,8 +10,9 @@ type Props = {
 };
 
 const AllBlogPosts = ({ data }: Props): ReactNode => (
-  <Layout>
+  <Layout title="Blogs at Nodejs">
     <main className="blog-grid-container">
+      {!data.blogs.edges.length && <h1>No Blog Posts yet</h1>}
       {data.blogs.edges.map(node => (
         <BlogCard key={node.node.fields.slug} data={node} />
       ))}
@@ -22,7 +23,10 @@ const AllBlogPosts = ({ data }: Props): ReactNode => (
 export const pageQuery = graphql`
   query AllBlogPostsPageQuery {
     blogs: allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/blog/" } }
+      filter: {
+        fileAbsolutePath: { regex: "/blog/" }
+        frontmatter: { title: { ne: "dummy" } }
+      }
       sort: { fields: [fields___date], order: DESC }
     ) {
       edges {
