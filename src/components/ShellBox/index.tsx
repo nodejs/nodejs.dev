cb880b625ed9e6aaf7d6796d908569e90acb46ac
Taimoor Farras <farrastaimoor@gmail.com>
2021-04-21 16:43:57 +0300

    fix issue changing only scss

--- src/components/ShellBox/index.tsx
+++ src/components/ShellBox/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useHover } from '../../hooks';
 
 import './ShellBox.scss';
 
@@ -10,8 +9,6 @@ interface Props {
 
 const ShellBox = ({ children, textToCopy }: Props): JSX.Element => {
   const [copied, setCopied] = React.useState(false);
-  const codeRef = React.useRef<HTMLElement>(null);
-  const isHover = useHover(codeRef);
 
   React.useEffect((): (() => void) => {
     let timer: ReturnType<typeof setTimeout>;
@@ -29,21 +26,6 @@ const ShellBox = ({ children, textToCopy }: Props): JSX.Element => {
     };
   }, [copied]);
 
-  React.useEffect((): void => {
-    const element = codeRef.current;
-    const { scrollWidth = 0, offsetWidth = 0 } = element || {};
-
-    if (element && scrollWidth > offsetWidth) {
-      if (isHover) {
-        element.style.overflowX = 'scroll';
-        element.style.paddingBottom = '0px';
-      } else {
-        element.style.overflowX = 'hidden';
-        element.style.paddingBottom = '.5em';
-      }
-    }
-  }, [isHover]);
-
   return (
     <pre className="shell-box">
       <div className="shell-box-top">
@@ -60,7 +42,7 @@ const ShellBox = ({ children, textToCopy }: Props): JSX.Element => {
           {copied ? <i className="material-icons">done</i> : null}
         </button>
       </div>
-      <code ref={codeRef}>{children}</code>
+      <code>{children}</code>
     </pre>
   );
 };
--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,3 +1,2 @@
 export { useApiData } from './useApiDocs';
 export { useReleaseHistory } from './useReleaseHistory';
-export { default as useHover } from './useHover';
