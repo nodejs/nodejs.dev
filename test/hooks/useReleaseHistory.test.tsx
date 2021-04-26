b85d1a74af097bd23f6c6e06b8c25451ad1901a8
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2021-04-26 18:55:57 +0300

    test(useReleaseHistory): Add tests

---
+++ test/hooks/useReleaseHistory.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import fetchMock from 'jest-fetch-mock';
+import {
+  getStaticReleaseData,
+  useReleaseHistory,
+} from '../../src/hooks/useReleaseHistory';
+
+describe('useReleaseHistory', () => {
+  it('should export getStaticReleaseData function', () => {
+    expect(getStaticReleaseData).toBeDefined();
+    expect(getStaticReleaseData().length).toBeGreaterThan(0);
+  });
+
+  describe('useReleaseHistory hook tests', () => {
+    const ReleaseHistoryRenderer = (): JSX.Element => {
+      const data = useReleaseHistory();
+      return <>{JSON.stringify(data)}</>;
+    };
+
+    beforeEach(() => {
+      fetchMock.enableMocks();
+    });
+
+    afterEach(() => {
+      fetchMock.resetMocks();
+    });
+
+    it('should retrieve data from correct endpoint', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({ mockedData: [] }));
+      const { getByText } = render(<ReleaseHistoryRenderer />);
+
+      await waitFor(() => {
+        expect(getByText(/mockedData/i)).toBeInTheDocument();
+        expect(fetchMock.mock.calls[0][0]).toBe(
+          'https://nodejs.org/dist/index.json'
+        );
+      });
+    });
+  });
+});
