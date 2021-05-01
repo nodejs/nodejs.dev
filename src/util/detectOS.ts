624214603e05034d5cbe6236cb216d4cc5e913e3
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2021-05-02 01:54:18 +0300

    Revert detectOS changes

--- src/util/detectOS.ts
+++ src/util/detectOS.ts
@@ -9,28 +9,28 @@ export enum UserOS {
 }
 
 export function detectOS(): UserOS {
-  let OS: UserOS;
-
-  switch (typeof navigator !== 'undefined') {
-    case navigator.appVersion.includes('Win'):
-      OS = UserOS.WIN;
-      break;
-    case navigator.appVersion.includes('Mac'):
-      OS = UserOS.MAC;
-      break;
-    case navigator.appVersion.includes('X11'):
-      OS = UserOS.UNIX;
-      break;
-    case navigator.appVersion.includes('Linux'):
-      OS = UserOS.LINUX;
-      break;
-    case navigator.appVersion.includes('Mobi'):
-      OS = UserOS.MOBILE;
-      break;
-    default:
-      OS = UserOS.UNKNOWN;
-      break;
+  let OS = UserOS.UNKNOWN;
+  if (typeof window !== 'undefined') {
+    switch (typeof navigator !== 'undefined') {
+      case navigator.appVersion.includes('Win'):
+        OS = UserOS.WIN;
+        break;
+      case navigator.appVersion.includes('Mac'):
+        OS = UserOS.MAC;
+        break;
+      case navigator.appVersion.includes('X11'):
+        OS = UserOS.UNIX;
+        break;
+      case navigator.appVersion.includes('Linux'):
+        OS = UserOS.LINUX;
+        break;
+      case navigator.appVersion.includes('Mobi'):
+        OS = UserOS.MOBILE;
+        break;
+      default:
+        OS = UserOS.UNKNOWN;
+        break;
+    }
   }
-
   return OS;
 }
