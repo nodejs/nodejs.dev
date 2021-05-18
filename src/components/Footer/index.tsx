9e37b50a0ccbede32df3779b37cd1ce227cfb200
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-05-18 16:24:51 +0530

    Resolved conflicts

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -26,12 +26,9 @@ function Footer(): JSX.Element {
             </a>
           </li>
           <li>
-            <a
-              className="footer__link"
-              href="https://nodejs.org/en/about/privacy/"
-            >
+            <Link className="footer__link" to="/privacy">
               Privacy Policy
-            </a>
+            </Link>
           </li>
           <li>
             <a
