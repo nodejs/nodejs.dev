741988bc77b51e65fcc7fcf758b4027d46d97dc3
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-05-21 01:10:58 +0530

    Revert "Resolved conflicts"
    This reverts commit 9e37b50a0ccbede32df3779b37cd1ce227cfb200.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -25,6 +25,9 @@ function Footer(): JSX.Element {
           </li>
           <li>
           <Link className="footer__link" to="/privacy">
+              className="footer__link"
+              href="https://nodejs.org/en/about/privacy/"
+            >
               Privacy Policy
           </Link>
           </li>
