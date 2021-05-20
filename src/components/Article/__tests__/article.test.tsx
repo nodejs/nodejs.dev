14f105eb353f9e4fcfafd6ae88e5c46bcb248d2b
Parth <img_2019045@iiitm.ac.in>
2021-05-20 13:26:45 +0530

    Fixed linting issues

--- src/components/Article/__tests__/article.test.tsx
+++ src/components/Article/__tests__/article.test.tsx
@@ -45,15 +45,8 @@ describe('Article component', () => {
   });
 
   it('renders correctly', () => {
-    const {
-      title,
-      description,
-      html,
-      next,
-      previous,
-      authors,
-      relativePath,
-    } = getArticleProps();
+    const { title, description, html, next, previous, authors, relativePath } =
+      getArticleProps();
 
     const { container } = render(
       <Article
@@ -71,15 +64,8 @@ describe('Article component', () => {
   });
 
   it('renders correctly in case html ref is null', () => {
-    const {
-      title,
-      description,
-      html,
-      next,
-      previous,
-      authors,
-      relativePath,
-    } = getArticleProps();
+    const { title, description, html, next, previous, authors, relativePath } =
+      getArticleProps();
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
@@ -101,14 +87,8 @@ describe('Article component', () => {
   });
 
   it('renders correctly in blog mode', () => {
-    const {
-      title,
-      description,
-      html,
-      next,
-      previous,
-      relativePath,
-    } = getArticleProps();
+    const { title, description, html, next, previous, relativePath } =
+      getArticleProps();
 
     const authors = [
       {
@@ -136,15 +116,8 @@ describe('Article component', () => {
   });
 
   it('should delete first article title from history', () => {
-    const {
-      title,
-      description,
-      html,
-      next,
-      previous,
-      authors,
-      relativePath,
-    } = getArticleProps();
+    const { title, description, html, next, previous, authors, relativePath } =
+      getArticleProps();
 
     window.history.replaceState = jest.fn();
 
@@ -170,7 +143,7 @@ describe('Article component', () => {
                 },
               },
             ],
-            (jest.fn() as unknown) as IntersectionObserver
+            jest.fn() as unknown as IntersectionObserver
           );
         }),
       };
@@ -198,15 +171,8 @@ describe('Article component', () => {
   });
 
   it('should save scrolled title position in history', () => {
-    const {
-      title,
-      description,
-      html,
-      next,
-      previous,
-      authors,
-      relativePath,
-    } = getArticleProps();
+    const { title, description, html, next, previous, authors, relativePath } =
+      getArticleProps();
 
     window.history.replaceState = jest.fn();
 
@@ -232,7 +198,7 @@ describe('Article component', () => {
                 },
               },
             ],
-            (jest.fn() as unknown) as IntersectionObserver
+            jest.fn() as unknown as IntersectionObserver
           );
         }),
       };
@@ -260,15 +226,8 @@ describe('Article component', () => {
   });
 
   it('should skip non-scrolled title from saving in history', () => {
-    const {
-      title,
-      description,
-      html,
-      next,
-      previous,
-      authors,
-      relativePath,
-    } = getArticleProps();
+    const { title, description, html, next, previous, authors, relativePath } =
+      getArticleProps();
 
     window.history.replaceState = jest.fn();
 
@@ -294,7 +253,7 @@ describe('Article component', () => {
                 },
               },
             ],
-            (jest.fn() as unknown) as IntersectionObserver
+            jest.fn() as unknown as IntersectionObserver
           );
         }),
       };
@@ -316,15 +275,8 @@ describe('Article component', () => {
   });
 
   it('should scroll into last viewed article position', () => {
-    const {
-      title,
-      description,
-      html,
-      next,
-      previous,
-      authors,
-      relativePath,
-    } = getArticleProps();
+    const { title, description, html, next, previous, authors, relativePath } =
+      getArticleProps();
 
     // add scroll position to history
     window.history.replaceState(
