866fa71766a68869b0ccc1d46aa5e9b16a26f3a8
Becky Chan <beckychan@ibm.com>
2021-06-03 17:55:40 -0700

    remove learn button, remove displaydownload from Hero

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Hero from '../components/Hero';
 import Layout from '../components/Layout';
 
 interface Props {
@@ -12,8 +11,8 @@ export default function NotFoundPage({ location }: Props): JSX.Element {
 
   return (
     <Layout title={title} location={location} description={description}>
-      <Hero title={title} displayDownload={false} />
-      <main style={{ width: '100%', textAlign: 'center' }}>
+      <main style={{ width: '100%', textAlign: 'center', marginTop: '150px' }}>
+        <h1> {title}</h1>
         <p>
           The page you&apos;re trying to access does not exist. Go back to the
           Homepage or find what you&apos;re looking for in the menu.
