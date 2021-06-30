b9ae573d37954bbb8e012b4a27852b77ecb3d7f4
Ben Halverson <benhalverson33@gmail.com>
2021-06-29 23:16:42 -0700

    refactor: 💡 Display security banner on homepage

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Banner.scss';
 
 const bannerLink =
-  'https://nodejs.org/en/blog/vulnerability/february-2020-security-releases/';
+  'https://nodejs.org/en/blog/vulnerability/july-2021-security-releases/';
 
 /**
  * The banner is used for displaying upcoming Nodejs events and important announcements ex. security updates
@@ -25,7 +25,7 @@ const Banner = (): JSX.Element => {
             Blog post
           </button>
         </a>
-        New security releases now available for all release lines
+        New security releases to be made available July 1st, 2021
       </p>
     </div>
   );
