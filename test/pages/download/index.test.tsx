ff05df8875b892b960a15b29c81e97f7019abace
Lance McCluskey <t.lance.mccluskey@gmail.com>
2021-07-16 07:32:39 -0500

    test: adding tests pm and download page

---
+++ test/pages/download/index.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import DownloadPage, {
+  DownloadNodeReleases,
+} from '../../../src/pages/download';
+import {
+  createNodeReleasesData,
+  createNodeReleasesDataDetail,
+} from '../../__fixtures__/page';
+import '../../__mocks__/intersectionObserverMock';
+
+const mockNodeReleasesData = createNodeReleasesData();
+const mockNodeReleasesDataDetail = createNodeReleasesDataDetail();
+const mockNodeReleasesLTSNPMVersion = mockNodeReleasesDataDetail.map(
+  ({ lts, version, npm }) => ({
+    lts,
+    version,
+    npm,
+  })
+);
+
+const nodeReleaseData: DownloadNodeReleases = {
+  nodeReleases: {
+    nodeReleasesData: mockNodeReleasesData,
+    nodeReleasesLTSNPMVersion: mockNodeReleasesLTSNPMVersion,
+  },
+};
+
+describe('Download page', () => {
+  it('renders correctly', () => {
+    const { container } = render(
+      <DownloadPage location={window.location} data={nodeReleaseData} />
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should handle LTS to Current switch', () => {
+    const { container } = render(
+      <DownloadPage location={window.location} data={nodeReleaseData} />
+    );
+
+    fireEvent.click(screen.getAllByText('Current')[0]);
+
+    expect(container).toMatchSnapshot();
+  });
+});
