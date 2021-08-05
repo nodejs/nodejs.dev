748799a5df0aaf952ac9cbf57e5b7917e2a3fa62
Thomas Lance McCluskey <40124399+lancemccluskey@users.noreply.github.com>
2021-08-04 20:25:22 -0500

    refactor: updating gatsby node file (#1662)
    * refactor: updating gatsby node file
    
    * refactor: createPage needs context property
    
    Co-authored-by: Rodion Abdurakhimov <rodion-arr@users.noreply.github.com>
    Co-authored-by: Ben Halverson <7907232+benhalverson@users.noreply.github.com>

---
+++ util-node/getNodeReleasesData.js
@@ -0,0 +1,54 @@
+const fetch = require('node-fetch');
+const getReleaseStatus = require('./getReleaseStatus');
+
+async function getNodeReleasesData() {
+  try {
+    const releasesDataDetailURL = 'https://nodejs.org/dist/index.json';
+    const releasesDataURL =
+      'https://raw.githubusercontent.com/nodejs/Release/main/schedule.json';
+
+    const releasesDataDetailResponse = await fetch(releasesDataDetailURL);
+    const releasesDataDetailResult = await releasesDataDetailResponse.json();
+
+    const releasesDataResponse = await fetch(releasesDataURL);
+    const releasesDataResult = await releasesDataResponse.json();
+
+    const mappedReleasesDataDetail = releasesDataDetailResult
+      .map(release => ({
+        ...release,
+        lts: release.lts || '',
+      }))
+      .slice(0, 50);
+
+    const filteredReleasesData = Object.keys(releasesDataResult)
+      .filter(key => {
+        const release = releasesDataResult[key];
+        const end = new Date(release.end);
+        return end >= new Date();
+      })
+      .map(key => {
+        const release = releasesDataResult[key];
+
+        return {
+          endOfLife: release.end,
+          maintenanceLTSStart: release.maintenance || '',
+          activeLTSStart: release.lts || '',
+          codename: release.codename || '',
+          initialRelease: release.start,
+          release: key,
+          status: getReleaseStatus(release),
+        };
+      });
+
+    const nodeReleasesData = {
+      nodeReleasesDataDetail: mappedReleasesDataDetail,
+      nodeReleasesData: filteredReleasesData,
+    };
+
+    return nodeReleasesData;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+module.exports = getNodeReleasesData;
