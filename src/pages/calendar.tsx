5813985e5c1485c8431d601d19355a4c54fe7fab
Lance McCluskey <t.lance.mccluskey@gmail.com>
2021-08-26 11:32:35 -0500

    refactor: Adding moment-timezone to fix tz issue

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, SyntheticEvent } from 'react';
-import moment from 'moment';
+// import moment from 'moment';
+import moment from 'moment-timezone';
 import dompurify from 'dompurify';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { Calendar, Formats, momentLocalizer } from 'react-big-calendar';
 import { usePopper } from 'react-popper';
 import Layout from '../components/Layout';
 import { getCalendarURL } from '../util/gcalUtils';
@@ -9,7 +10,16 @@ import { CalendarEvent } from '../types';
 import { useGCalAPI } from '../hooks/useGCalAPI';
 import '../styles/calendar.scss';
 
-const localizer = momentLocalizer(moment);
+// const meow = moment();
+// BigCalendar.tz = moment.tz.guess();
+// moment.tz.setDefault();
+// const localizer = momentLocalizer(moment);
+// const currentTimezone = moment.tz.guess();
+// const getMoment = (timezone = currentTimezone) => {
+//   const m = (...args) => moment.tz(...args, timezone);
+//   m.localeData = moment.localeData;
+//   return m;
+// };
 
 export default function NodeCalendarPage(): JSX.Element {
   const [events, setEvents] = useGCalAPI();
@@ -57,16 +67,103 @@ export default function NodeCalendarPage(): JSX.Element {
   };
 
   const startAccessor = (event: CalendarEvent): Date => {
+    // console.log(event.startTime.utcOffset());
+    // console.log(
+    //   '🚀 -> file: calendar.tsx -> line 128 -> startAccessor -> event.startTime.utcOffset()',
+    //   event.startTime.utcOffset()
+    // );
+    if (
+      event.startTime.date() === 26 &&
+      event.startTime.year() === 2021 &&
+      event.startTime.month() === 7
+    ) {
+      console.log(
+        '🚀 -> file: calendar.tsx -> line 70 -> startAccessor -> event',
+        event
+      );
+      console.log(
+        '🚀 -> file: calendar.tsx -> line 70 -> startAccessor -> event offset',
+        event.startTime.utc(true).utcOffset()
+      );
+    }
     return event.startTime.toDate();
+    // const date = new Date(event.startTime.toLocaleString());
+    // console.log(
+    //   '🚀 -> file: calendar.tsx -> line 85 -> startAccessor -> date',
+    //   date
+    // );
+
+    // return date;
+    // return moment.tz(event.startTime.toISOString()).toDate();
   };
 
   const endAccessor = (event: CalendarEvent): Date => {
     return event.endTime.toDate();
+    // return new Date(event.endTime.toLocaleString());
+    // return moment.tz(event.endTime.toISOString()).toDate();
   };
 
   const onView = () => {
     setReferenceElement(null);
   };
+
+  useEffect(() => {
+    const augEvents = events?.filter(event => event.startTime.month() === 7);
+    console.log(
+      '🚀 -> file: calendar.tsx -> line 74 -> NodeCalendarPage -> events',
+      augEvents
+    );
+  }, [events]);
+
+  const formats: Formats = {
+    dateFormat: 'dd',
+    timeGutterFormat: (date, culture, local) => {
+      return moment(date).toLocaleString();
+    },
+    dayFormat: (date, culture, local) => local!.format(date, 'DDD', culture!),
+    // dayRangeHeaderFormat: ({ start, end }, culture, local) =>
+    //   `${local!.format(start, '',culture)} - ${local!.format(end, { date: 'short' }, culture)}`,
+    eventTimeRangeFormat: (range, culture, local) => {
+      console.log(
+        '🚀 -> file: calendar.tsx -> line 96 -> NodeCalendarPage -> culture',
+        culture
+      );
+      console.log(
+        '🚀 -> file: calendar.tsx -> line 96 -> NodeCalendarPage -> range',
+        range
+      );
+      // return local!.format(
+      // console.log(
+      //   '🚀 -> file: calendar.tsx -> line 117 -> NodeCalendarPage -> local?.formats',
+      //   local?.formats
+      // );
+      const eventTimeRange = `${moment(range.start.toLocaleString()).format(
+        'h:mma'
+      )} - ${moment(range.end.toLocaleString()).format('h:mma')}`;
+      console.log(
+        '🚀 -> file: calendar.tsx -> line 108 -> eventTimeRange -> eventTimeRange',
+        eventTimeRange
+      );
+
+      return 'foobar';
+    },
+  };
+
+  // const currentTimezone = moment.tz.guess();
+  // const getMoment = (timezone = currentTimezone) => {
+  //   // eslint-disable-next-line
+  //   // @ts-ignore
+  //   const m = (...rest: any[]) => moment.tz(...rest, timezone);
+  //   m.localeData = moment.localeData;
+  //   return m;
+  // };
+
+  const mom = () => {
+    moment.tz.setDefault();
+    return moment;
+  };
+  // TODO: I think the root issue has to do with how this localizer is formatting dates
+  const localizer = momentLocalizer(mom());
 
   return (
     <Layout title="Node.js Calendar" description="Node.js upcoming events">
@@ -79,7 +176,8 @@ export default function NodeCalendarPage(): JSX.Element {
         endAccessor={endAccessor}
         onNavigate={onNavigate}
         onView={onView}
-        views={['month']}
+        // formats={formats}
+        // views={['month']}
         popup
       />
       {referenceElement && (
