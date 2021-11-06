fbbb674773b9e43f4eee399fbc9f977932ba14bd
Artem Zakharchenko <kettanaito@gmail.com>
2021-11-06 02:28:39 +0100

    chore: Add "svgMock" jest transformer, update snapshots

---
+++ test/__mocks__/svgMock.tsx
@@ -0,0 +1,15 @@
+import React, { SVGProps } from 'react';
+
+export const ReactComponent = React.forwardRef<
+  SVGSVGElement,
+  SVGProps<SVGSVGElement>
+>((props, ref) => (
+  <svg
+    ref={ref}
+    /* eslint-disable-next-line react/jsx-props-no-spreading */
+    {...props}
+  />
+));
+ReactComponent.displayName = 'SvgrMock';
+
+export default 'SvgUrl';
