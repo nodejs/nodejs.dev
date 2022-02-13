597a2442e80abe65f32635c88fa345ecbda61f1a
ashiishme <iamashiishy@gmail.com>
2022-02-13 21:15:09 +0100

    feat: add recent post component on single blog

---
+++ src/components/RecentPosts/index.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import { BlogMetaData } from '../../types';
+
+import './RecentPosts.scss';
+
+interface Props {
+  posts: BlogMetaData[];
+}
+
+const RecentPosts = ({ posts }: Props) => {
+  return (
+    <div className="recent-posts">
+      <h2 className="recent-posts-header">Recent Posts</h2>
+      <ul className="recent-post-list">
+        {posts.map(post => {
+          const {
+            node: {
+              frontmatter: { title },
+              fields: { slug },
+            },
+          } = post;
+          return (
+            <li key={slug} className="recent-post-item">
+              <Link activeClassName="recent-post-item-active" to={slug}>
+                {title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default RecentPosts;
