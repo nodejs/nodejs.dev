2e471a10bdfdbdae58f844af2e72fef59260e473
Strek <ssharishkumar@gmail.com>
2022-06-22 17:05:04 +0530

    build: add redirects generator script

---
+++ scripts/generateRedirects.js
@@ -0,0 +1,34 @@
+const { resolve } = require('path');
+const { readFileSync, writeFile } = require('fs');
+const redirects = require('../util-node/redirects');
+
+const firebaseConfig = resolve(__dirname, '../firebase.json');
+
+const firebaseJSON = JSON.parse(readFileSync(firebaseConfig));
+
+let firebaseRedirects = [];
+
+firebaseRedirects = Object.entries(redirects).map(([key, value]) => ({
+  ...firebaseRedirects,
+  source: key,
+  destination: value,
+  type: '301',
+}));
+
+writeFile(
+  firebaseConfig,
+  JSON.stringify(
+    {
+      ...firebaseJSON,
+      redirects: firebaseRedirects,
+    },
+    null,
+    2
+  ),
+  err => {
+    if (err) console.error('error writing redirects', err);
+    else {
+      console.log('redirects written successfully\n');
+    }
+  }
+);
