b40c0d48ee988b94e4cadd267a987b48a8b3c52e
Claudio Wunder <cwunder@gnome.org>
2022-06-26 17:10:25 +0200

    refactor(create-doc): updated logic on create-doc to use the categoryName field

--- util-node/createDocPages.js
+++ util-node/createDocPages.js
@@ -4,21 +4,15 @@ function createDocPages(edges) {
 
   edges.forEach(({ node }, index) => {
     const {
-      fields: { slug },
-      frontmatter: { title, section, category: categoryObject },
+      fields: { slug, categoryName },
+      frontmatter: { title, section },
       parent: { relativePath },
       fileAbsolutePath,
     } = node;
 
-    const categoryName = categoryObject ? categoryObject.name : '';
-
     let previousNodeData = null;
     const previousNode = index === 0 ? undefined : edges[index - 1].node;
-    if (
-      previousNode &&
-      previousNode.frontmatter.category &&
-      previousNode.frontmatter.category.name === categoryName
-    ) {
+    if (previousNode && previousNode.fields.categoryName === categoryName) {
       previousNodeData = {
         slug: previousNode.fields.slug,
         title: previousNode.frontmatter.title,
@@ -28,11 +22,7 @@ function createDocPages(edges) {
     let nextNodeData = null;
     const nextNode =
       index === edges.length - 1 ? undefined : edges[index + 1].node;
-    if (
-      nextNode &&
-      nextNode.frontmatter.category &&
-      nextNode.frontmatter.category.name === categoryName
-    ) {
+    if (nextNode && nextNode.fields.categoryName === categoryName) {
       nextNodeData = {
         slug: nextNode.fields.slug,
         title: nextNode.frontmatter.title,
