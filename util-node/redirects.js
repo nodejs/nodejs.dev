c60cae17fe9be728d85666d8c03429414a9280d4
Claudio Wunder <cwunder@gnome.org>
2022-08-07 13:49:06 +0200

    chore(redirects): remove non-needed index redirect

--- util-node/redirects.js
+++ util-node/redirects.js
@@ -1,10 +1,5 @@
-const { defaultLanguage } = require('./locales');
-
 // This file is used to define all the changed slugs from original nodejs.org site to our new site.
-// And other redirections
 module.exports = {
   // Redirects Node.js Get Involved to Community
   '/en/get-involved': '/community',
-  // Redirects no localized pages to the default language
-  '/': `/${defaultLanguage}`,
 };
