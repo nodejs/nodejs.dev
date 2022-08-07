f8bce36a54c7f739ff218c0ca0e43fd1ef6a07b0
Claudio Wunder <cwunder@gnome.org>
2022-08-07 13:50:25 +0200

    utils(browser-language): created util to detect the browser language

---
+++ src/util/detectLanguage.ts
@@ -0,0 +1,24 @@
+import { localesAsString } from '../../util-node/locales';
+
+const localeKeysAsArray = localesAsString.split(' ');
+
+export const detectLanguage = () => {
+  // Some of these languages have locale codes, eg.: en-US, en-DE so we want to break those down
+  const browserAvailableLocales = window.navigator.languages
+    ? window.navigator.languages
+    : [window.navigator.language];
+
+  const browserAvailableLanguages = browserAvailableLocales
+    .map(locale => locale.toLowerCase().split('-'))
+    .flat();
+
+  const firstMatchingLanguage = browserAvailableLanguages.find(language =>
+    localeKeysAsArray.includes(language)
+  );
+
+  if (firstMatchingLanguage) {
+    return firstMatchingLanguage;
+  }
+
+  return null;
+};
