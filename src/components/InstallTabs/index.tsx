ec7e62fceba2584f08098dff670dbf01fa6e9f0d
Claudio Wunder <cwunder@gnome.org>
2022-08-16 21:53:06 +0200

    fix(ssr): fixes some of the ssr errors caused by not using hooks

--- src/components/InstallTabs/index.tsx
+++ src/components/InstallTabs/index.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { detectOS, UserOS } from '../../util/detectOS';
-import './InstallTabs.scss';
-
+import { UserOS } from '../../util/detectOS';
+import { useDetectOs } from '../../hooks/useDetectOs';
 import WindowsPanel from './WindowsPanel';
 import MacOSPanel from './MacOSPanel';
 import LinuxPanel from './LinuxPanel';
 
+import './InstallTabs.scss';
+
+const getOSPanel = (userOS: UserOS): JSX.Element => {
+  switch (userOS) {
+    case UserOS.MAC:
+      return (
+        <>
+          <TabPanel>
+            <MacOSPanel />
+          </TabPanel>
+          <TabPanel>
+            <WindowsPanel />
+          </TabPanel>
+          <TabPanel>
+            <LinuxPanel />
+          </TabPanel>
+        </>
+      );
+    case UserOS.LINUX:
+    case UserOS.UNIX:
+      return (
+        <>
+          <TabPanel>
+            <LinuxPanel />
+          </TabPanel>
+          <TabPanel>
+            <MacOSPanel />
+          </TabPanel>
+          <TabPanel>
+            <WindowsPanel />
+          </TabPanel>
+        </>
+      );
+    default:
+      return (
+        <>
+          <TabPanel>
+            <WindowsPanel />
+          </TabPanel>
+          <TabPanel>
+            <MacOSPanel />
+          </TabPanel>
+          <TabPanel>
+            <LinuxPanel />
+          </TabPanel>
+        </>
+      );
+  }
+};
+
 const InstallTabs = (): JSX.Element | null => {
-  const userOS = detectOS();
+  const { userOS } = useDetectOs();
 
   const os = {
     win: 'Windows (Chocolatey)',
@@ -24,55 +73,9 @@ const InstallTabs = (): JSX.Element | null => {
     UNKNOWN: [os.win, os.mac, os.linux],
   };
 
-  function panelSwitch(): JSX.Element {
-    switch (userOS) {
-      case UserOS.MAC:
-        return (
-          <>
-            <TabPanel>
-              <MacOSPanel />
-            </TabPanel>
-            <TabPanel>
-              <WindowsPanel />
-            </TabPanel>
-            <TabPanel>
-              <LinuxPanel />
-            </TabPanel>
-          </>
-        );
-      case UserOS.LINUX:
-      case UserOS.UNIX:
-        return (
-          <>
-            <TabPanel>
-              <LinuxPanel />
-            </TabPanel>
-            <TabPanel>
-              <MacOSPanel />
-            </TabPanel>
-            <TabPanel>
-              <WindowsPanel />
-            </TabPanel>
-          </>
-        );
-      default:
-        return (
-          <>
-            <TabPanel>
-              <WindowsPanel />
-            </TabPanel>
-            <TabPanel>
-              <MacOSPanel />
-            </TabPanel>
-            <TabPanel>
-              <LinuxPanel />
-            </TabPanel>
-          </>
-        );
-    }
-  }
+  const panelSwitch = useMemo(() => getOSPanel(userOS), [userOS]);
 
-  return installTabSystems[userOS] !== undefined ? (
+  return (
     <div className="install">
       <Tabs>
         <div className="install__header">
@@ -90,10 +93,10 @@ const InstallTabs = (): JSX.Element | null => {
             <Tab key={system.toString()}>{system}</Tab>
           ))}
         </TabList>
-        {panelSwitch()}
+        {panelSwitch}
       </Tabs>
     </div>
-  ) : null;
+  );
 };
 
 export default InstallTabs;
