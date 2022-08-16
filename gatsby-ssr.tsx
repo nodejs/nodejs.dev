f6df06a287bc43a924eaa313845176352296601e
Claudio Wunder <cwunder@gnome.org>
2022-08-16 21:16:47 +0200

    feat(react-intl): safe approach in case pageContext doesn't exist

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import ReactIntlProvider from './src/containers/ReactIntl';
+import { defaultLanguage } from './util-node/locales';
 import type { WrapPageElementSSR } from './src/types';
 
 // eslint-disable-next-line import/prefer-default-export
 export const wrapPageElement: WrapPageElementSSR = ({ element, props }) => {
-  const { locale, intlMessages } = props.pageContext;
+  const { locale = defaultLanguage, intlMessages = {} } = props.pageContext;
 
   // eslint-disable-next-line react/jsx-props-no-spreading
   return (
