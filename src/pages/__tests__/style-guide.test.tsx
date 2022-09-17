a940722b138d44306ccd326f437d56e82bfe1c01
Emma Dawson <emma.l.dawson@gmail.com>
2022-09-17 21:19:18 +0200

    test: Add a11y test to style-guide page

--- src/pages/__tests__/style-guide.test.tsx
+++ src/pages/__tests__/style-guide.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import StyleGuidePage from '../style-guide';
+import { axe, toHaveNoViolations } from 'jest-axe';
+
+expect.extend(toHaveNoViolations);
 
 describe('StyleGuide page', () => {
   it('renders correctly', () => {
     const { container } = render(<StyleGuidePage />);
     expect(container).toMatchSnapshot();
+  });
+  it('has no accessibility violations', async () => {
+    const { container } = render(<StyleGuidePage />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
