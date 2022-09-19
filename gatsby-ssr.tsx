9e0300dc8e4741b3de2891f397e3e16ce2350af3
Nick Schonning <nschonni@gmail.com>
2022-09-19 00:03:44 -0400

    chore: remove unused ESLint directives (#2772)

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -3,12 +3,10 @@ import ReactIntlProvider from './src/containers/ReactIntl';
 import { defaultLanguage, defaultMessages } from './locales';
 import type { WrapPageElementSSR } from './src/types';
 
-// eslint-disable-next-line import/prefer-default-export
 export const wrapPageElement: WrapPageElementSSR = ({ element, props }) => {
   const { locale = defaultLanguage, intlMessages = defaultMessages } =
     props.pageContext;
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
   return (
     <ReactIntlProvider locale={locale} messages={intlMessages}>
       {element}
