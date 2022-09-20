328e0e214f70268f676517adb5e63e6b770955a1
Ben Halverson <7907232+benhalverson@users.noreply.github.com>
2022-09-19 20:56:56 -0700

    added local plugin to fix windows error (#2773)
    * added local plugin to fix windows error
    
    * updated package.json for local plugin
    
    * added readme and set the plugin package.json to private

---
+++ plugins/redirect/redirect.js
@@ -0,0 +1,18 @@
+module.exports = function redirect(toPath) {
+  let url = toPath.trim();
+
+  const hasProtocol = url.includes('://');
+  if (!hasProtocol) {
+    const hasLeadingSlash = url.startsWith('/');
+    if (!hasLeadingSlash) {
+      url = `/${url}`;
+    }
+
+    const resemblesFile = url.includes('.');
+    if (!resemblesFile) {
+      url = `${url}/`.replace(/\/\/+/g, '/');
+    }
+  }
+
+  return `<meta http-equiv="refresh" content="0; URL='${url}'" />`;
+};
