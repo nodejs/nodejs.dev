69f27e735fbe22c88b4de83c941df462108f4005
Ben Halverson <7907232+benhalverson@users.noreply.github.com>
2022-10-02 02:58:00 -0700

    fix: 🐛 redirect old links to /en/learn (#2809)
    Co-authored-by: Claudio Wunder <cwunder@gnome.org>

--- redirects.js
+++ redirects.js
@@ -12,4 +12,18 @@ module.exports = {
   '/about/trademark/': 'https://trademark-policy.openjsf.org/',
   // Redirects the old `/docs` path to `/api`
   '/docs/*': '/api/',
+  '/en/learn/the-packagejson-guide/': '/en/learn/',
+  '/en/learn/how-to-exit-from-a-nodejs-program/': '/en/learn/',
+  '/en/learn/where-does-npm-install-the-packages/': '/en/learn/',
+  '/en/learn/get-http-request-body-data-using-nodejs/': '/en/learn/',
+  '/en/learn/a-brief-history-of-nodejs/': '/en/learn/',
+  '/en/learn/nodejs-file-paths/': '/en/learn/',
+  '/learn/asynchronous-work/blocking-vs-non-blocking':
+    'https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Introducing',
+  '/learn/discover-javascript-timers':
+    'https://developer.mozilla.org/en-US/docs/Web/API/setTimeout',
+  '/learn/modern-asynchronous-javascript-with-async-and-await':
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises',
+  '/learn/the-nodejs-event-loop':
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises',
 };
