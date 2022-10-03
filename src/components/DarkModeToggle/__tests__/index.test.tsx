aca4e5c58a766e623dcc99aec7ef292d6655b451
Vignesh Muthukumar <vickymhs@gmail.com>
2022-10-02 20:37:42 -0400

    Test: Add unit tests for DarkModeToggle component

---
+++ src/components/DarkModeToggle/__tests__/index.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import DarkModeToggle from "../index";
+
+describe('DarkModeToggle Component', ()=> {
+  it('render dark mode toggle', ()=> {
+    const { container } = render(<DarkModeToggle />);
+    expect(container).toMatchSnapshot();
+  });
+})
