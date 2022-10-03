49a36aaae82038cccab935af5c285e6fa7df3c90
forgeRW <20483211+forgeRW@users.noreply.github.com>
2022-10-03 09:57:30 -0400

    Test: clean up unit tests for VersionSelector
    * Clean up references to window.location in unit tests for VersionSelector in ApiComponents

--- src/components/ApiComponents/Components/VersionSelector/__tests__/index.test.tsx
+++ src/components/ApiComponents/Components/VersionSelector/__tests__/index.test.tsx
@@ -32,55 +32,54 @@ describe('VersionSelector component', (): void => {
     expect(container).toMatchSnapshot();
   });
 
-  it('redirects to selected version', async () => {
-    const locationBackup = window.location;
-    // eslint-disable-next-line
-    delete (window as any).location;
-    // eslint-disable-next-line
-    (window as any).location = {
-      href: jest.fn(),
-    };
+  describe('redirection', () => {
+    let location: Location | null | undefined;
+    let mockLocation;
 
-    const apiAvailableVersions = [version];
-    render(
-      <VersionSelector
-        releases={releases}
-        selectedRelease={version}
-        apiAvailableVersions={apiAvailableVersions}
-        currentPage={currentPage}
-      />
-    );
-    const optionComponent = screen.getByRole('combobox');
-    fireEvent.change(optionComponent, { target: { value: version } });
-    const expectedUrl = `/api/${version}/${currentPage}`;
-    expect(window.location.href).toEqual(expectedUrl);
+    beforeEach(() => {
+      location = window.location;
+      mockLocation = jest.fn();
+      // eslint-disable-next-line
+      delete (window as any).location;
+      // eslint-disable-next-line
+      (window as any).location = mockLocation;
+    });
 
-    window.location = locationBackup;
-  });
+    it('redirects to selected version', async () => {
+      const apiAvailableVersions = [version];
+      render(
+        <VersionSelector
+          releases={releases}
+          selectedRelease={version}
+          apiAvailableVersions={apiAvailableVersions}
+          currentPage={currentPage}
+        />
+      );
+      const optionComponent = screen.getByRole('combobox');
+      fireEvent.change(optionComponent, { target: { value: version } });
+      const expectedUrl = `/api/${version}/${currentPage}`;
+      expect(window.location.href).toEqual(expectedUrl);
+    });
 
-  it('redirects to latest version', async () => {
-    const locationBackup = window.location;
-    // eslint-disable-next-line
-    delete (window as any).location;
-    // eslint-disable-next-line
-    (window as any).location = {
-      href: jest.fn(),
-    };
+    it('redirects to latest version', async () => {
+      const apiAvailableVersions = ['v1'];
+      render(
+        <VersionSelector
+          releases={releases}
+          selectedRelease={version}
+          apiAvailableVersions={apiAvailableVersions}
+          currentPage={currentPage}
+        />
+      );
+      const optionComponent = screen.getByRole('combobox');
+      fireEvent.change(optionComponent, { target: { value: version } });
+      const expectedUrl = `https://nodejs.org/docs/latest-${version}.x/api/index.html`;
+      expect(window.location.href).toEqual(expectedUrl);
+    });
 
-    const apiAvailableVersions = ['v1'];
-    render(
-      <VersionSelector
-        releases={releases}
-        selectedRelease={version}
-        apiAvailableVersions={apiAvailableVersions}
-        currentPage={currentPage}
-      />
-    );
-    const optionComponent = screen.getByRole('combobox');
-    fireEvent.change(optionComponent, { target: { value: version } });
-    const expectedUrl = `https://nodejs.org/docs/latest-${version}.x/api/index.html`;
-    expect(window.location.href).toEqual(expectedUrl);
-
-    window.location = locationBackup;
+    afterEach(() => {
+      // eslint-disable-next-line
+      (window as any).location = location;
+    });
   });
 });
