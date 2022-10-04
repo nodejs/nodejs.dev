a2ce0e92f0ce16ae577f0831c4aa22f0013fbbfe
Ciao Advertising <manis16@outlook.com>
2022-10-04 16:42:49 +0500

    fix linting errors duplicate keys

--- redirects.js
+++ redirects.js
@@ -7,10 +7,11 @@ module.exports = {
     'https://github.com/nodejs/TSC/blob/main/WORKING_GROUPS.md',
   '/releases/': '/about/releases/',
   '/security/': '/about/security/',
+
   // Outside Redirects
   '/trademark/': 'https://trademark-policy.openjsf.org/',
   '/about/trademark/': 'https://trademark-policy.openjsf.org/',
-  '/about/trademark/': 'https://trademark-policy.openjsf.org/',
+
   // Redirects the old `/docs` path to `/api`
   '/docs/*': '/api/',
   '/storybook/*': '/',
@@ -23,7 +24,6 @@ module.exports = {
   '/learn/how-to-exit-from-a-nodejs-program/': '/learn/',
   '/learn/where-does-npm-install-the-packages/': '//learn/',
   '/learn/get-http-request-body-data-using-nodejs/': '/learn/',
-  '/learn/a-brief-history-of-nodejs/': '/learn/',
   '/learn/nodejs-file-paths/': '/learn/',
   '/learn/asynchronous-work/blocking-vs-non-blocking':
     'https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Introducing',
@@ -45,7 +45,6 @@ module.exports = {
   '/blog/weekly-updates/2015/07/31/weekly-update.2015-07-31': '/blog',
   '/blog/weekly-updates/2016/02/15/weekly-update.2016-02-15': '/blog',
   '/blog/weekly-updates/2016/03/07/weekly-update.2016-03-07': '/blog',
-  '/blog/weekly-updates/2016/08/15/weekly-update.2016-08-15': '/blog',
   '/blog/announcements/2021/10/07/retiring-the-node-js-community-committee':
     '/blog',
   '/blog/announcements/2022/02/14/nodejs-trademarks-transferred-to-openjs-foundation':
@@ -62,26 +61,20 @@ module.exports = {
   '/learn/asynchronous-flow-control/': '/learn/',
   '/learn/build-an-http-server/': '/learn/',
   '/learn/discover-javascript-timers/': '/learn/',
-  '/learn/ecmascript-2015-es6-and-beyond/': '/learn/',
-  '/learn/expose-functionality-from-a-nodejs-file-using-exports/':
-    '/learn/',
-  '/learn/find-the-installed-version-of-an-npm-package/': '/learn/',
+  '/learn/expose-functionality-from-a-nodejs-file-using-exports/': '/learn/',
   '/learn/how-to-log-an-object-in-nodejs/': '/learn/',
   '/learn/how-to-read-environment-variables-from-nodejs/': '/learn/',
   '/learn/how-to-use-the-nodejs-repl/': '/learn/',
   '/learn/install-an-older-version-of-an-npm-package/': '/learn/',
   '/learn/making-http-requests-with-nodejs/': '/learn/',
-  '/learn/modern-asynchronous-javascript-with-async-and-await/':
-    '/learn/',
+  '/learn/modern-asynchronous-javascript-with-async-and-await/': '/learn/',
   '/learn/nodejs-accept-arguments-from-the-command-line/': '/learn/',
-  '/learn/nodejs-buffers/': '/learn/',
   '/learn/nodejs-streams/': '/learn/',
   '/learn/nodejs-with-typescript/': '/learn/',
   '/learn/nodejs-with-webassembly/': '/learn/',
   '/learn/output-to-the-command-line-using-nodejs/': '/learn/',
   '/learn/overview-of-blocking-vs-non-blocking/': '/learn/',
   '/learn/reading-files-with-nodejs/': '/learn/',
-  '/learn/run-nodejs-scripts-from-the-command-line/': '/learn/',
   '/learn/semantic-versioning-using-npm/': '/learn/',
   '/learn/the-nodejs-event-loop/': '/learn/',
   '/learn/the-nodejs-events-module/': '/learn/',
@@ -90,22 +83,16 @@ module.exports = {
   '/learn/understanding-javascript-promises/': '/learn/',
   '/learn/understanding-setimmediate/': '/learn/',
   '/learn/uninstalling-npm-packages/': '/learn/',
-  '/learn/update-all-the-nodejs-dependencies-to-their-latest-version/':
-    '/learn/',
   '/learn/working-with-folders-in-nodejs': '/learn/',
   '/learn/a-brief-history-of-nodejs/': '/learn',
-  '/learn/build-an-http-server/': '/learn',
   '/learn/ecmascript-2015-es6-and-beyond/': '/learn',
   '/learn/error-handling-in-nodejs/': '/learn',
   '/learn/find-the-installed-version-of-an-npm-package/': '/learn',
   '/learn/javascript-asynchronous-programming-and-callbacks/': '/learn',
-  '/learn/making-http-requests-with-nodejs/': '/learn',
   '/learn/nodejs-buffers/': '/learn',
-  '/learn/nodejs-file-paths/': '/learn',
   '/learn/npm-dependencies-and-devdependencies/': '/learn',
   '/learn/run-nodejs-scripts-from-the-command-line/': '/learn',
   '/learn/the-nodejs-event-emitter/': '/learn',
-  '/learn/the-nodejs-events-module/': '/learn',
   '/learn/the-nodejs-path-module/': '/learn',
   '/learn/update-all-the-nodejs-dependencies-to-their-latest-version/':
     '/learn',
