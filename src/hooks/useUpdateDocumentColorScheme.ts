9a310c5efeddcf29b11662f88e2315645aa7cf86
Kwan Jun Wen <40173716+junwen-k@users.noreply.github.com>
2022-10-06 18:20:42 +0800

    feat(useSetDocumentColorScheme): set document color scheme css hook (#2880)
    * feat(useSetDocumentColorScheme): add hook
    
    * feat(useSetDocumentColorScheme): use hook
    
    * feat(useUpdateDocumentColorScheme): update hook
    
    * test(useUpdateDocumentColorScheme): add test
    
    * fix(useUpdateDocumentColorScheme): fix lint
    
    * feat(useUpdateDocumentColorScheme): use hook
    
    Co-authored-by: Claudio Wunder <cwunder@gnome.org>

---
+++ src/hooks/useUpdateDocumentColorScheme.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+export const useUpdateDocumentColorScheme = (
+  defaultTheme: string | null = null
+) => {
+  const [currentTheme, setCurrentTheme] = useState(defaultTheme);
+
+  useEffect(() => {
+    // This is responsible for setting the color-scheme of the scroll-bars
+    if (typeof document === 'object' && document.documentElement) {
+      document.documentElement.style['color-scheme'] = currentTheme;
+    }
+  }, [currentTheme]);
+
+  return (theme: string | null) => setCurrentTheme(theme);
+};
