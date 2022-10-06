923e1d72eaa4998d659e44b6f0f90e1e1a452c6c
Vignesh Muthukumar <vickymhs@gmail.com>
2022-10-05 21:00:46 -0400

    Test: Added test to switch dark-light-dark mode by mocking the dark-mode package component

--- src/components/DarkModeToggle/__tests__/index.test.tsx
+++ src/components/DarkModeToggle/__tests__/index.test.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import DarkModeToggle from '../index';
+
+let mockCurrentTheme = '';
+const mockToggleTheme = jest.fn().mockImplementation((newTheme: string) => {
+  mockCurrentTheme = newTheme === 'light' ? 'dark' : 'light';
+});
+
+// mock dark mode module for controlling dark mode HOC behavior
+jest.mock('@skagami/gatsby-plugin-dark-mode', () => ({
+  __esModule: true,
+  useTheme: () => [mockCurrentTheme, mockToggleTheme],
+}));
 
 describe('DarkModeToggle Component', () => {
   it('render dark mode toggle', () => {
     const { container } = render(<DarkModeToggle />);
     expect(container).toMatchSnapshot();
+  });
+
+  it('switches dark theme to light theme', async () => {
+    mockCurrentTheme = 'dark';
+    render(<DarkModeToggle />);
+    const toggle = await screen.findByText('Toggle Dark Mode');
+    await userEvent.click(toggle);
+  });
+
+  it('switches light theme to dark theme', async () => {
+    mockCurrentTheme = 'light';
+    render(<DarkModeToggle />);
+    const toggle = await screen.findByText('Toggle Dark Mode');
+    await userEvent.click(toggle);
   });
 });
