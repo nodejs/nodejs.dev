e560be5dcf17580cda82ff8d07b9ee00ad919285
junwen-k <junwenkwan97@gmail.com>
2022-10-07 00:01:42 +0800

    test(copyTextToClipboard): update tests

--- src/util/__tests__/copyTextToClipboard.test.ts
+++ src/util/__tests__/copyTextToClipboard.test.ts
@@ -5,7 +5,22 @@ describe('copyTextToClipboard', () => {
     expect(copyTextToClipboard).toBeDefined();
   });
 
-  it('should call navigator.clipboard.writeText with `test`', async () => {
+  it('should return `false` when copy failed', async () => {
+    const navigatorClipboardWriteTextSpy = jest
+      .fn()
+      .mockImplementation(() => Promise.reject());
+
+    Object.defineProperty(window.navigator, 'clipboard', {
+      writable: true,
+      value: {
+        writeText: navigatorClipboardWriteTextSpy,
+      },
+    });
+
+    expect(await copyTextToClipboard('test')).toBe(false);
+  });
+
+  it('should return `true` when successfully copied', async () => {
     const navigatorClipboardWriteTextSpy = jest
       .fn()
       .mockImplementation(() => Promise.resolve());
@@ -18,6 +33,21 @@ describe('copyTextToClipboard', () => {
     });
 
     expect(await copyTextToClipboard('test')).toBe(true);
+  });
+
+  it('should call clipboard API with `test`', () => {
+    const navigatorClipboardWriteTextSpy = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve());
+
+    Object.defineProperty(window.navigator, 'clipboard', {
+      writable: true,
+      value: {
+        writeText: navigatorClipboardWriteTextSpy,
+      },
+    });
+
+    copyTextToClipboard('test');
 
     expect(navigatorClipboardWriteTextSpy).toHaveBeenCalledTimes(1);
     expect(navigatorClipboardWriteTextSpy).toHaveBeenCalledWith('test');
