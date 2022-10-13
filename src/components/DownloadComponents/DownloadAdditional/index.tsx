3d8e1080532a6ce6ff6f2d38dfa3cea5763f440a
Claudio Wunder <cwunder@gnome.org>
2022-10-13 08:31:33 +0200

    fix(tsc): removed unused import

--- src/components/DownloadComponents/DownloadAdditional/index.tsx
+++ src/components/DownloadComponents/DownloadAdditional/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FC } from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import DownloadableItem from './DownloadableItem';
 import DownloadToggle from '../DownloadToggle';
