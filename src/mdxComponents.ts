7cb81449038392e72397c72e94b02f0e5b9929e0
wishee <84540554+vaishnav-mk@users.noreply.github.com>
2022-11-01 02:08:55 +0530

    Removing the Info component as BlockQuotes already exist
    Signed-off-by: wishee <84540554+vaishnav-mk@users.noreply.github.com>

--- src/mdxComponents.ts
+++ src/mdxComponents.ts
@@ -9,7 +9,6 @@ export const mdxComponents = {
   table: ArticleComponents.Table,
   blockquote: ArticleComponents.BlockQuote,
   Alert: ArticleComponents.Alert,
-  Info: ArticleComponents.Info,
 };
 
 export const apiMdxComponents = {
