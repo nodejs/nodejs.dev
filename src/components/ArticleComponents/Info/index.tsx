6e16a096540c4f86b5a0297948077d4218d7bbd3
wishee <84540554+vaishnav-mk@users.noreply.github.com>
2022-11-01 01:44:14 +0530

    Removing the Info component as BlockQuotes already exist
    Signed-off-by: wishee <84540554+vaishnav-mk@users.noreply.github.com>

--- src/components/ArticleComponents/Info/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './index.module.scss';
-
-const Info = ({ children }: React.PropsWithChildren): JSX.Element => (
-  <div className={styles.info}>{children}</div>
-);
-
-export default Info;
