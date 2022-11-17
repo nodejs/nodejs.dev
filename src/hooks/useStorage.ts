656a6e6cbbbb9b0aff06c09b1549a1cd2f3068eb
Claudio Wunder <cwunder@gnome.org>
2022-11-17 14:26:20 +0100

    hotfix(language-detection): fix language detection (#3021)
    * hotfix(language-detection): hook was disabled
    
    * chore(dependencies): updated dependencies
    
    * feat(storage): introduced a better storage layer
    
    * fix(tests): updated and fixed tests

---
+++ src/hooks/useStorage.ts
@@ -0,0 +1,59 @@
+import { useEffect } from 'react';
+
+// Creates a permanent storage to be used between all the parts of the Application
+// So it doesn't matter where this hook gets created it will be global.
+// Note.: This could be moved within a Provider to move it to a React-isolated context-tree instead of a global runtime one.
+export const appStorage = new Map<string, string>();
+
+const syncWithBrowser = () => {
+  if (typeof localStorage === 'object') {
+    return {
+      sendToBrowser: (k: string, v: string) => localStorage.setItem(k, v),
+      getFromBrowser: (k: string) => localStorage.getItem(k) || undefined,
+    };
+  }
+
+  return {
+    sendToBrowser: () => undefined,
+    getFromBrowser: () => undefined,
+  };
+};
+
+export const useStorage = () => {
+  const { sendToBrowser, getFromBrowser } = syncWithBrowser();
+
+  const setItem = (key: string, value: unknown) =>
+    appStorage.set(key, JSON.stringify(value));
+
+  const getItem = (key: string) => {
+    // Attempts to retrieve items from the App Storage, but if they don't exist yet
+    // They might exist in the Browser Local Storage. This is useful as we retrieve things
+    // From the Web Storage API only if they don't exist in the App Storage
+    const storageItem = appStorage.get(key) || getFromBrowser(key);
+    const parsedValue = storageItem ? JSON.parse(storageItem) : undefined;
+
+    // If a said item doesn't exist in the App Storage we will create one
+    // On the App Storage that will either be the value from the Web Storage if it exists
+    // Or `undefined`. This is an interesting approach as it will ensure that all requested keys
+    // at one point will exist in the Web Storage, and keeps things in sync.
+    if (!appStorage.has(key)) {
+      setItem(key, parsedValue);
+    }
+
+    return parsedValue;
+  };
+
+  const flushToStorage = () =>
+    [...appStorage.entries()].forEach(([k, v]) => sendToBrowser(k, v));
+
+  useEffect(() => {
+    // Instead of flushing to the localStorage on every change, we flush once the Application
+    // is being unloaded, so it allows us to update all changes at once instead of constantly
+    // invoking the Web Storage APIs for every change we need, as the "authoritative storage" is the app itself
+    window.addEventListener('unload', flushToStorage);
+
+    return () => window.removeEventListener('unload', flushToStorage);
+  }, []);
+
+  return { getItem, setItem };
+};
--- src/providers/FeatureToggles/index.tsx
+++ src/providers/FeatureToggles/index.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useMemo } from 'react';
+import { useStorage } from '../../hooks/useStorage';
 
 type Props = { children?: React.ReactNode };
 
@@ -7,26 +8,20 @@ const FEATURE_FLAGS_STORAGE = 'node_featureFlags';
 export const FeatureToggleContext = createContext<string[]>([]);
 
 export const FeatureToggleProvider: React.FC<Props> = ({ children }) => {
+  const { getItem } = useStorage();
+
   const featureFlags = useMemo(() => {
-    try {
-      const storageItem = localStorage.getItem(FEATURE_FLAGS_STORAGE) || '[]';
+    const parsedItem = getItem(FEATURE_FLAGS_STORAGE) || [];
 
-      const parsedItem = JSON.parse(storageItem);
-
-      // We want to guarantee the parsedItem is truthy and and Array
-      if (parsedItem && Array.isArray(parsedItem)) {
-        // Lastly we filter non-string objects as we want to avoid code injection such as XSS
-        // from starting from our feature flags
-        return [...parsedItem].filter(f => typeof f === 'string' && f.length);
-      }
-
-      return [];
-    } catch (__noop) {
-      // We don't care if the JSON.parse or localStorage.getItem fail
-      // as we will just disable the feature flags then
-      return [];
+    // We want to guarantee the parsedItem is truthy and and Array
+    if (parsedItem && Array.isArray(parsedItem)) {
+      // Lastly we filter non-string objects as we want to avoid code injection such as XSS
+      // from starting from our feature flags
+      return [...parsedItem].filter(f => typeof f === 'string' && f.length);
     }
-  }, []);
+
+    return [];
+  }, [getItem]);
 
   return (
     <FeatureToggleContext.Provider value={featureFlags}>
