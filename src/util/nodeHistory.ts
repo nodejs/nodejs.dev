76777af7c3ddc640558bf93554ba95cd81346947
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2022-12-09 16:29:50 +0100

    Update nodeHistory.ts

--- src/util/nodeHistory.ts
+++ src/util/nodeHistory.ts
@@ -1,5 +1,6 @@
 export const getAgeInYears = (date: string): number =>
-  new Date(new Date().getTime() - new Date(date).getTime()).getFullYear() - 1970;
+  new Date(new Date().getTime() - new Date(date).getTime()).getFullYear() -
+  1970;
 
 export const isBirthdayToday = (date: string): boolean =>
   new Date().getDate() === new Date(date).getDate() &&
