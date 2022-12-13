1c93c6892bacf597b2314ee69ed6548e81abac02
Shanmughapriyan S <priyanshan03@gmail.com>
2022-12-13 16:39:25 +0530

    FIx: add fallback img for author

--- src/components/ArticleComponents/AuthorList/Author/index.tsx
+++ src/components/ArticleComponents/AuthorList/Author/index.tsx
@@ -5,19 +5,16 @@ import styles from './index.module.scss';
 interface Props {
   index: number;
   username: string;
-  size: string;
+  githubImgLink: string;
 }
 
 const Author = ({
   index,
   username,
-  size = '64',
   intl,
+  githubImgLink,
 }: Props & WrappedComponentProps) => {
-  // Clean up username and build links.
-  const githubUserName = username.trim();
-  const githubLink = `https://github.com/${githubUserName}`;
-  const githubImgLink = `https://github.com/${githubUserName}.png?size=${size}`;
+  const githubLink = `https://github.com/${username}`;
 
   // If it's the first author then no margin left.
   const mleft = index === 0 ? { marginLeft: 0 } : {};
--- src/components/ArticleComponents/AuthorList/index.tsx
+++ src/components/ArticleComponents/AuthorList/index.tsx
@@ -1,23 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import Author from './Author';
 import styles from './index.module.scss';
+import githubFallbackImg from '../../../images/placeholder-img.png';
+
+const GITHUB_IMG_SIZE = '60';
 
 interface Props {
   authors: string[];
 }
 
+const authorImgCache = {};
+
+function checkAuthorImg(username: string): Promise<string> {
+  if (authorImgCache[username]) {
+    return authorImgCache[username];
+  }
+  return new Promise(resolve => {
+    let img: HTMLImageElement | null = new Image();
+    const githubImgLink = `https://github.com/${username.trim()}.png?size=${GITHUB_IMG_SIZE}`;
+
+    img.onload = () => {
+      resolve(githubImgLink);
+      authorImgCache[username] = githubImgLink;
+      img = null;
+    };
+
+    img.onerror = () => {
+      resolve(githubFallbackImg);
+      authorImgCache[username] = githubFallbackImg;
+      img = null;
+    };
+
+    img.src = githubImgLink;
+  });
+}
+
 const AuthorList = ({ authors }: Props): JSX.Element => {
+  const [githubImgLinks, setGithubImgLinks] = useState<string[]>([]);
+
+  useEffect(() => {
+    (async function authorImgLinks() {
+      const links: string[] = await Promise.all(
+        authors.map(author => checkAuthorImg(author.trim()))
+      );
+      setGithubImgLinks(links);
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   if (authors.length) {
     return (
       <div className={styles.authorList}>
         <FormattedMessage id="containers.authorList.title" />
         <ul>
-          {authors.map(
-            (author, i): JSX.Element => (
-              <Author index={i} username={author} key={author} size="60" />
-            )
-          )}
+          {authors.map((author, i): JSX.Element => {
+            // Clean up author
+            const username = author.trim();
+            return (
+              <Author
+                index={i}
+                username={username}
+                key={username}
+                githubImgLink={authorImgCache[username] ?? githubImgLinks[i]}
+              />
+            );
+          })}
         </ul>
       </div>
     );
