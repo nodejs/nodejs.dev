ec5a98e128d631abc584603cda0ac05e7f1671f0
Shanmughapriyan S <priyanshan03@gmail.com>
2022-12-30 22:14:10 +0530

    Chore: add new hook `useCopyToClipboard`

---
+++ src/hooks/__tests__/useCopyToClipboard.test.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import { useCopyToClipboard } from '../useCopyToClipboard';
+
+describe('useCopyToClipboard', () => {
+  const HookRenderer = ({ text }: { text: string }): JSX.Element => {
+    const [copied, copyText] = useCopyToClipboard();
+
+    return (
+      <button onClick={() => copyText(text)} type="button">
+        {copied ? 'copied' : 'copy'}
+      </button>
+    );
+  };
+
+  it('should have `copy` text when failed', async () => {
+    const navigatorClipboardWriteTextSpy = jest
+      .fn()
+      .mockImplementation(() => Promise.reject());
+
+    Object.defineProperty(window.navigator, 'clipboard', {
+      writable: true,
+      value: {
+        writeText: navigatorClipboardWriteTextSpy,
+      },
+    });
+
+    render(<HookRenderer text="test copy" />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(button).toHaveTextContent('copy');
+  });
+
+  it('should change to `copied` when copy succeeded', async () => {
+    const navigatorClipboardWriteTextSpy = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve());
+
+    Object.defineProperty(window.navigator, 'clipboard', {
+      writable: true,
+      value: {
+        writeText: navigatorClipboardWriteTextSpy,
+      },
+    });
+
+    render(<HookRenderer text="test copy" />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(button).toHaveTextContent('copied');
+    });
+    await waitFor(() => {
+      expect(button).toHaveTextContent('copy');
+    });
+  });
+
+  it('should call clipboard API with `test` once', () => {
+    const navigatorClipboardWriteTextSpy = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve());
+
+    Object.defineProperty(window.navigator, 'clipboard', {
+      writable: true,
+      value: {
+        writeText: navigatorClipboardWriteTextSpy,
+      },
+    });
+
+    render(<HookRenderer text="test" />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(navigatorClipboardWriteTextSpy).toHaveBeenCalledTimes(1);
+    expect(navigatorClipboardWriteTextSpy).toHaveBeenCalledWith('test');
+  });
+});
---
+++ src/hooks/useCopyToClipboard.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+export const copyToClipboard = async (value: string) => {
+  try {
+    await navigator.clipboard.writeText(value);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const useCopyToClipboard = (): [
+  boolean,
+  (text: string) => Promise<boolean>
+] => {
+  const [copied, setCopied] = useState(false);
+
+  async function copyText(text: string) {
+    const isCopied = await copyToClipboard(text);
+    setCopied(isCopied);
+    return isCopied;
+  }
+
+  useEffect(() => {
+    if (!copied) return undefined;
+
+    const timerId = setTimeout(() => setCopied(false), 300);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [copied]);
+
+
+  return [copied, copyText];
+};
