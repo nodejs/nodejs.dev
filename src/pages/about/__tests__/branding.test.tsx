b19f3a59a6dbc230375729acaba6d273bec1be7b
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-01-08 16:57:46 +0100

    feat(branding): add snapshots

--- src/pages/about/__tests__/branding.test.tsx
+++ src/pages/about/__tests__/branding.test.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
-import ResourcesPage from '..';
-import { createResourcesData } from '../../../__fixtures__/page';
+import BrandingPage from '..';
+import { createBrandingData } from '../../../__fixtures__/page';
 
-const mockData = createResourcesData();
+const mockData = createBrandingData();
 expect.extend(toHaveNoViolations);
 
-describe('Resources page', () => {
+describe('Branding page', () => {
   it('renders correctly', () => {
-    const { container } = render(<ResourcesPage data={mockData.data} />);
+    const { container } = render(<BrandingPage data={mockData.data} />);
     expect(container).toMatchSnapshot();
   });
   it('should have no a11y violations', async () => {
-    const { container } = render(<ResourcesPage data={mockData.data} />);
+    const { container } = render(<BrandingPage data={mockData.data} />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
