3f82ce40687fcee34056eae558b20bb1c70bd443
Steven <steven@ceriously.com>
2023-01-20 16:32:38 -0500

    fix: add catch and optional chaining

--- src/hooks/useDetectOs.ts
+++ src/hooks/useDetectOs.ts
@@ -8,19 +8,19 @@ export const useDetectOs = () => {
 
   useEffect(() => {
     setUserOS(detectOS);
-    try {
-      // This is necessary to detect Windows 11 on Edge.
-      // [MDN](https://developer.mozilla.org/en-US/docs/Web/API/NavigatorUAData/getHighEntropyValues)
-      // [MSFT](https://learn.microsoft.com/en-us/microsoft-edge/web-platform/how-to-detect-win11)
-      // @ts-expect-error no types yet because this API is experimental
-      navigator.userAgentData
-        .getHighEntropyValues(['bitness'])
-        .then((ua: { bitness: string }) => {
-          setBitness(ua.bitness);
-        });
-    } catch (e) {
-      // Ignore errors since not every browser supports this API
-    }
+
+    // This is necessary to detect Windows 11 on Edge.
+    // [MDN](https://developer.mozilla.org/en-US/docs/Web/API/NavigatorUAData/getHighEntropyValues)
+    // [MSFT](https://learn.microsoft.com/en-us/microsoft-edge/web-platform/how-to-detect-win11)
+    // @ts-expect-error no types for "userAgentData" because this API is experimental
+    navigator?.userAgentData
+      ?.getHighEntropyValues(['bitness'])
+      .then((ua: { bitness: string }) => {
+        setBitness(ua.bitness);
+      })
+      .catch(() => {
+        // Ignore errors since not every browser supports this API
+      });
   }, []);
 
   return {
