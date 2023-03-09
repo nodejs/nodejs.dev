5350eb2360a93c8e727822cc81f84ab4382dc6a8
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2023-03-09 22:17:40 +0100

    fix(ci)

--- util-node/getBannersData.js
+++ util-node/getBannersData.js
@@ -1,7 +1,5 @@
 const { nodeBannersData } = require('../apiUrls');
 
-// @TODO: In the future the Banners Data should be stored directly in this Repository
-// If this repository ever replaces the Nodejs.org repository
 function getBannersData() {
   // This function gets the current Nodejs.org website metadata
   // And retrieves the `banners` data to display on the website
